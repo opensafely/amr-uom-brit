b3ad41712708387a1aac50588548c592a94c59ec
v1kki213 <victoria.palin@manchester.ac.uk>
2021-09-10 13:36:34 +0100

    Update study_definition.py
    Adding some basic functions to define the study population - these will need amending to our specific protocol, but generating the .py file for creating the dataset.

--- analysis/study_definition.py
+++ analysis/study_definition.py
@@ -1,13 +1,76 @@
-from cohortextractor import StudyDefinition, patients, codelist, codelist_from_csv  # NOQA
+######################################
 
+# This script provides the formal specification of the study data that will be extracted from
+# the OpenSAFELY database.
 
-study = StudyDefinition(
-    default_expectations={
-        "date": {"earliest": "1900-01-01", "latest": "today"},
-        "rate": "uniform",
-        "incidence": 0.5,
-    },
-    population=patients.registered_with_one_practice_between(
-        "2019-02-01", "2020-02-01"
-    ),
+######################################
+
+# --- IMPORT STATEMENTS ---
+
+## Import code building blocks from cohort extractor package
+from cohortextractor import (
+    StudyDefinition,
+    patients,
+    #codelist_from_csv,
+    codelist,
+    #filter_codes_by_category,
+    #combine_codelists,
+    #Measure
 )
+
+## Import codelists from codelist.py (which pulls them from the codelist folder)
+#  from codelists import *
+
+## code from template (same as above)
+#from cohortextractor import StudyDefinition, patients, codelist, codelist_from_csv  # NOQA
+
+# DEFINE STUDY POPULATION ---
+
+## Define study time variables
+from datetime import datetime
+
+start_date = "2019-01-01"
+end_date = datetime.today().strftime('%Y-%m-%d')
+
+## Define study population and variables
+study = StudyDefinition(
+
+    # Configure the expectations framework
+    default_expectations={
+        "date": {"earliest": start_date, "latest": end_date},
+        "rate": "uniform",
+        "incidence": 0.1,
+    },
+    # Set index date to start date
+    index_date=start_date,
+    # Define the study population
+    population=patients.satisfying(
+        """
+        NOT has_died
+        AND
+        registered
+        AND
+        age
+        AND
+        has_follow_up_previous_year
+        AND
+        (sex = "M" OR sex = "F")
+        """,
+
+        has_died=patients.died_from_any_cause(
+            on_or_before="index_date",
+            returning="binary_flag",
+        ),
+
+        registered=patients.satisfying(
+            "registered_at_start",
+            registered_at_start=patients.registered_as_of("index_date"),
+        ),
+
+        has_follow_up_previous_year=patients.registered_with_one_practice_between(
+            start_date="index_date - 1 year",
+            end_date="index_date",
+            return_expectations={"incidence": 0.95},
+        ),
+
+    ),
