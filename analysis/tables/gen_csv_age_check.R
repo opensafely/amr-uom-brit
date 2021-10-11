4c69e725f9df392820570e85e877e979fe6ddc59
v1kki213 <victoria.palin@manchester.ac.uk>
2021-10-11 13:11:43 +0100

    generated a quintile table chek

--- analysis/tables/gen_csv_age_check.R
+++ analysis/tables/gen_csv_age_check.R
@@ -1,38 +1,19 @@
 
 # # # # # # # # # # # # # # # # # # # # #
 # This script:
-# Generate a tabl to show the age distributionof elderly population (65+)
-# Overall quantiles, by practice
+# Generate a table to show the age distribution of elderly population (65+)
+# Overall quantiles
 # # # # # # # # # # # # # # # # # # # # #
 
 ## Import libraries---
 library('tidyverse')
 library('dplyr')
-library('lubridate')
 
-# impoprt data
-df <- read_csv(
-  here::here("output", "input_elderly.csv.gz"),
-#  col_types = cols_only(
-    
-    # Identifier
-#    practice = col_integer(),
-    
-    # Outcomes
-#    antibacterial_prescriptions  = col_double(),
-#    population  = col_double(),
-#    value = col_double(),
-    
-    # Date
-#    date = col_date(format="%Y-%m-%d")
-    
-#  ),
-  na = character()
-  )
+setwd(here::here("output"))
 
-
-
+df <- read.csv(file=here::here("output", "input_elderly.csv.gz"))
 
 age_quant <- as.data.frame(quantile(df$age, na.rm=TRUE))
 colnames(age_quant)[1] <- "age"
-write.csv(age_quant, file="age_quant.csv", path=here::here("output","table"))
+
+write_csv(age_quant, here::here("output", "age_quant.csv"))
