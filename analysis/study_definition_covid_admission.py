4379dd96e39c5356fcb7d09a47aff38ef0f4fa4a
rriefu <rriefu@gmail.com>
2022-01-15 17:53:28 +0000

    edit study def.

--- analysis/study_definition_covid_admission.py
+++ analysis/study_definition_covid_admission.py
@@ -83,6 +83,60 @@ study = StudyDefinition(
         return_expectations={"date": {"earliest": "2020-03-01"}, "incidence" : 0.25},
     ),
 
+    ## Age
+    age=patients.age_as_of(
+        "patient_index_date",
+        return_expectations={
+            "rate": "universal",
+            "int": {"distribution": "population_ages"},
+            "incidence": 0.001
+        },
+    ),
+
+    ## Age categories
+    ## 0-4; 5-14; 15-24; 25-34; 35-44; 45-54; 55-64; 65-74; 75+
+    age_cat=patients.categorised_as(
+        {
+            "0":"DEFAULT",
+            "0-4": """ age >= 0 AND age < 5""",
+            "5-14": """ age >= 5 AND age < 15""",
+            "15-24": """ age >= 15 AND age < 25""",
+            "25-34": """ age >= 25 AND age < 35""",
+            "35-44": """ age >= 35 AND age < 45""",
+            "45-54": """ age >= 45 AND age < 55""",
+            "55-64": """ age >= 55 AND age < 65""",
+            "65-74": """ age >= 65 AND age < 75""",
+            "75+": """ age >= 75 AND age < 120""",
+        },
+        return_expectations={
+            "rate": "universal",
+            "category": {
+                "ratios": {
+                    "0": 0,
+                    "0-4": 0.12, 
+                    "5-14": 0.11,
+                    "15-24": 0.11,
+                    "25-34": 0.11,
+                    "35-44": 0.11,
+                    "45-54": 0.11,
+                    "55-64": 0.11,
+                    "65-74": 0.11,
+                    "75+": 0.11,
+                }
+            },
+        },
+    ),
+
+    
+    ## Sex
+    sex=patients.sex(
+        return_expectations={
+            "rate": "universal",
+            "category": {"ratios": {"M": 0.49, "F": 0.51}},
+        }
+    ),
+
+
 
     # observation end date
     ## de-register after start date
