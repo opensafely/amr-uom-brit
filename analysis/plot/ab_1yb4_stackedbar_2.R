95899dd2d2450994ecd7d6ab2c3957e3850c407b
v1kki213 <victoria.palin@manchester.ac.uk>
2022-02-08 17:54:28 +0000

    fixing row bind in # prior ABs 12m b4
    removed data table until can fix issue

--- analysis/plot/ab_1yb4_stackedbar_2.R
+++ analysis/plot/ab_1yb4_stackedbar_2.R
@@ -9,6 +9,7 @@
 ## Import libraries---
 library("tidyverse") 
 library("ggplot2")
+library('plyr')
 library('dplyr')
 library('lubridate')
 library('stringr')
@@ -40,11 +41,8 @@ for (i in seq_along(csvFiles)){
 }
 
 # combine list -> data.table/data.frame
-df_input <- rbindlist(temp, fill=TRUE)
+df_input <- plyr::ldply(temp, data.frame)
 rm(temp,csvFiles,i)# remove temporary list
-
-## select rows of interest
-#df_input <- select(df_input, age, sex, region, ethnicity, antibacterial_12mb4, date)
 
 df_input$date <- as.Date(df_input$date)
 df_input$cal_mon <- month(df_input$date)
@@ -62,8 +60,8 @@ num_pats <- as.numeric(dim(df)[1])
  
 ### make variable for categorising num ABs in 12m before
 ### group_by  month --- removed grouping by practice!!!
-df_gp <- df %>% group_by(cal_mon, cal_year) %>%
-   mutate(ab_cat = case_when(antibacterial_12mb4 >0 & antibacterial_12mb4 <4 ~ 2,
+df_gp <- df %>% dplyr::group_by(cal_mon, cal_year) %>%
+  dplyr::mutate(ab_cat = case_when(antibacterial_12mb4 >0 & antibacterial_12mb4 <4 ~ 2,
                              antibacterial_12mb4 >3 & antibacterial_12mb4 <7 ~ 3,
                              antibacterial_12mb4 >=7 ~ 4,
                              antibacterial_12mb4 == 0 ~1)) 
@@ -71,15 +69,15 @@ df_gp$ab_cat <- as.factor(df_gp$ab_cat)
 
 ### add labels to levels
 df_gp$`Prior ABs` <- factor(df_gp$ab_cat, labels=c("0", "1-3", "4-6", "7+"))
-
-### calculate % for each ab_category in each  
+#summary(df_gp$`Prior ABs`)
+### calculate % for each ab_category   
 ### by dividing by 'nrows' in groups to get population by month
-df_percent <- df_gp %>% group_by(cal_mon, cal_year) %>%
-   mutate(mon_listsize = n())
+df_percent <- df_gp %>% dplyr::group_by(cal_mon, cal_year) %>%
+  dplyr::mutate(mon_listsize = n())
 ### group by ab cat to work out percentage by category using practice listsize
-df_per_abgp <- df_percent %>% group_by(cal_mon, cal_year, `Prior ABs`) %>%
-   mutate(num_abcats = n()) %>%
-   mutate(percentgp = (num_abcats/mon_listsize)*100)
+df_per_abgp <- df_percent %>% dplyr::group_by(cal_mon, cal_year, `Prior ABs`) %>%
+  dplyr::mutate(num_abcats = n()) %>%
+  dplyr::mutate(percentgp = (num_abcats/mon_listsize)*100)
 
 prior12m_line <- ggplot(df_per_abgp, aes(x=date, y=percentgp, group=`Prior ABs`)) +
                   geom_line(aes(linetype=`Prior ABs`, colour=`Prior ABs`))+
@@ -87,7 +85,7 @@ prior12m_line <- ggplot(df_per_abgp, aes(x=date, y=percentgp, group=`Prior ABs`)
                   scale_y_continuous(limits = c(0,100))+
                   theme(axis.text.x=element_text(angle=60,hjust=1))+
                   labs(title = "Distribution of population level of prior antibiotic use (12m before) over time",
-                       x = "", y = "Percentage",
+                       x = "", y = "Percentage (%)",
                        caption = paste("Data from", num_pats,"patients"))
 ggsave(
   plot= prior12m_line,
@@ -95,16 +93,16 @@ ggsave(
 )
 
 # generate data table 
-dt_counts <- df_per_abgp %>% group_by(date) %>% count(`Prior ABs`) %>%
-  group_by(date)%>% mutate(totalobs = sum(n)) %>%
-  mutate(perc_per_month = round((n / totalobs)*100, digits=2))
-
-dt_counts_gender <- df_per_abgp %>% group_by(date, sex) %>% count(`Prior ABs`) %>%
-  group_by(date)%>% mutate(totalobs = sum(n)) %>%
-  mutate(perc_per_month = round((n / totalobs)*100, digits=2))
-
-dt_counts_combined <- rbind(dt_counts, dt_counts_gender)
-write_csv(dt_counts_combined, here::here("output", "prior_ab_by_month.csv"))
+# dt_counts <- df_per_abgp %>% group_by(date) %>% count(`Prior ABs`) %>%
+#   group_by(date)%>% mutate(totalobs = sum(n)) %>%
+#   mutate(perc_per_month = round((n / totalobs)*100, digits=2))
+# 
+# dt_counts_gender <- df_per_abgp %>% group_by(date, sex) %>% count(`Prior ABs`) %>%
+#   group_by(date)%>% mutate(totalobs = sum(n)) %>%
+#   mutate(perc_per_month = round((n / totalobs)*100, digits=2))
+# 
+# dt_counts_combined <- rbind(dt_counts, dt_counts_gender)
+# write_csv(dt_counts_combined, here::here("output", "prior_ab_by_month.csv"))
 
 ## plot by sex
 male_df <- filter(df_per_abgp, sex == "M")
