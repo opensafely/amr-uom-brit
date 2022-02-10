4d36c45463d8b85ae819e94c27d7aeb9dc501402
rriefu <rriefu@gmail.com>
2022-02-10 16:14:12 +0000

    transform dataframe

--- analysis/process/ab_recorded_indication_transform.R
+++ analysis/process/ab_recorded_indication_transform.R
@@ -15,114 +15,291 @@ library('lubridate')
 #library("ggpubr")
 
 rm(list=ls())
-setwd(here::here("output", "measures"))
+#setwd(here::here("output", "measures"))
+#setwd("/Users/yayang/Documents/GitHub/amr-uom-brit/output/measures")
 
 
-# # ### read data_ 2020-01-01 ro 2020-06-01
-# # ### mport patient-level data(study definition input.csv) to summarize antibiotics counts
-# # ############ loop reading multiple CSV files ################
-# # # read file list from input.csv
-csvFiles = list.files(pattern="input_antibiotics_2_", full.names = TRUE)
-csvFiles =csvFiles [1:6] # select half year
 
-temp <- vector("list", length(csvFiles))
 
-# # for (i in seq_along(csvFiles)){
-# #   filename <- csvFiles[i]
-# #   temp_df <- read_csv(filename)
-# #   filename <- basename(filename)
-# #   filename <-str_remove(filename, "input_antibiotics_2_")
-# #   filename <-str_remove(filename, ".csv.gz")
-  
-# #   #add to per-month temp df
-# #   temp_df$date <- filename
-# #   mutate(temp_df, date = as.Date(date, "%Y-%m-%d"))
-  
-# #   #add df to list
-# #   temp[[i]] <- temp_df
-# # }
+# file list
+csvFiles_19 = list.files(pattern="input_antibiotics_2019", full.names = FALSE)
+csvFiles_20 = list.files(pattern="input_antibiotics_2020_2020", full.names = FALSE)
+csvFiles_21 = list.files(pattern="input_antibiotics_2021", full.names = FALSE)
+csvFiles_22 = list.files(pattern="input_antibiotics_2022", full.names = FALSE)
 
-# # # combine list -> data.table/data.frame
-# # df <-plyr::ldply(temp, data.frame) 
-# # rm(temp,csvFiles,i,temp_df,filename)# remove temporary list
-
-# # filter all antibiotics users
-# df=df%>%filter(antibacterial_brit !=0)
-
-# ### remove last month data
-# last.date=max(df$date)
-# df=df%>% filter(date!=last.date)
-# first_mon=min(df$date)
-# last_mon= max(df$date)
-
-# df$date=as.Date(df$date)
-
-# read in files
-csvFiles = c("input_antibiotics_2020_2020-01-01.csv.gz","input_antibiotics_2020_2020-02-01.csv.gz","input_antibiotics_2020_2020-03-01.csv.gz",
-"input_antibiotics_2020_2020-04-01.csv.gz","input_antibiotics_2020_2020-05-01.csv.gz","input_antibiotics_2020_2020-06-01.csv.gz",
-"input_antibiotics_2020_2020-07-01.csv.gz","input_antibiotics_2020_2020-08-01.csv.gz","input_antibiotics_2020_2020-09-01.csv.gz",
-"input_antibiotics_2020_2020-10-01.csv.gz","input_antibiotics_2020_2020-11-01.csv.gz","input_antibiotics_2020_2020-12-01.csv.gz")
-datelist= c("2020-01-01","2020-02-01","2020-03-01","2020-04-01","2020-05-01","2020-06-01","2020-07-01","2020-08-01","2020-09-01","2020-10-01","2020-11-01","2020-12-01")
+# date list
+date_19= seq(as.Date("2019-01-01"), as.Date("2019-12-01"), "month")
+date_20= seq(as.Date("2020-01-01"), as.Date("2020-12-01"), "month")
+date_21= seq(as.Date("2021-01-01"), as.Date("2021-12-01"), "month")
+date_22= seq(as.Date("2022-01-01"), as.Date("2022-12-01"), "month")
 
 # variables names list
-prevalent_check=paste0("prevalent_AB_date_",rep(1:10))
-ab_count_10=paste0("AB_date_",rep(1:10),"_count") # change to binary flag
-ab_category=paste0("AB_date_",rep(1:10),"_indication")
+prevalent_check=paste0("prevalent_AB_date_",rep(1:12))
+#ab_count_12=paste0("AB_date_",rep(1:12),"_count") # change to binary flag
+ab_category=paste0("AB_date_",rep(1:12),"_indication")
 indications=c("uti","lrti","urti","sinusits","otmedia","ot_externa","asthma","cold","cough","copd","pneumonia","renal","sepsis","throat","uncoded")
-ab_date_10=paste0("AB_date_",rep(1:10))
-
-# #replace NA with "uncoded" in AB_indication_1-10 columns
-# for (i in 1:10){
-#   df[,ab_category[i]]=ifelse(is.na(df[,ab_category[i]]),"uncoded", df[,ab_category[i]])}
+ab_date_12=paste0("AB_date_",rep(1:12))
 
 
-for (i in 1:6){
-######### 2020-01-01 ##############
-# read in one-month data
-df <- read_csv(
-  here::here("output", "measures", csvFiles[i]))
-
-# filter all antibiotics users
-df=df%>%filter(antibacterial_brit !=0)
-
-# create date column
-df$date=as.Date(datelist[i])
 
 
-#### patient/row --> prescription/row
 
-# ab_date_1-10
-df1=df%>%select(patient_id,age,sex,ab_date_10)
-colnames(df1)[4:13]=paste0("time",rep(1:10))
-df1.1=df1%>%gather(times,date,paste0("time",rep(1:10)))
-rm(df1)
-# prevalent_AB_date_1-10
-df2=df%>%select(patient_id,age,sex,prevalent_check)
-colnames(df2)[4:13]=paste0("time",rep(1:10))
-df2.1=df2%>%gather(times,prevalent,paste0("time",rep(1:10)))
-rm(df2)
-# "AB_date_count"1-10
-df3=df%>%select(patient_id,age,sex,ab_count_10)
-colnames(df3)[4:13]=paste0("time",rep(1:10))
-df3.1=df3%>%gather(times,count,paste0("time",rep(1:10)))
-rm(df3)
-# ab_category 1-10
-df4=df%>%select(patient_id,age,sex,ab_category)
-colnames(df4)[4:13]=paste0("time",rep(1:10))
-df4.1=df4%>%gather(times,infection,paste0("time",rep(1:10)))
-rm(df4)
 
-# merge
-DF=merge(df1.1,df2.1,by=c("patient_id","age","sex","times"))
-DF=merge(DF,df3.1,by=c("patient_id","age","sex","times"))
-DF=merge(DF,df4.1,by=c("patient_id","age","sex","times"))
+# transform dataset & create list 2019
+temp <- vector("list", length(csvFiles_19))
 
-# exclude observation without AB prescription date
-DF=DF%>%filter(!is.na(date))
-DF$date=as.Date(DF$date,origin="1970-01-01")
+for (i in seq_along(csvFiles_19)){
 
-write_rds(DF, here::here("output", "measures",paste0("ab_",datelist[i],".rds")))
+  # read in one-month data
+  df <- read_csv(
+    here::here("output", "measures", csvFiles_19[i]))
+  
+  # filter all antibiotics users
+  df=df%>%filter(antibacterial_brit !=0)
+  
+  # create date column
+  df$date=as.Date(date_19[i])
+  
+  
+  #### patient/row --> prescription/row
+  
+  # ab_date_1-12
+  df1=df%>%select(patient_id,age,sex,ab_date_12)
+  colnames(df1)[4:15]=paste0("time",rep(1:12))
+  df1.1=df1%>%gather(times,date,paste0("time",rep(1:12)))
+  rm(df1)
 
-rm(DF,df1.1,df2.1,df3.1,df4.1)
+  # prevalent_AB_date_1-10
+  df2=df%>%select(patient_id,age,sex,prevalent_check)
+  colnames(df2)[4:15]=paste0("time",rep(1:12))
+  df2.1=df2%>%gather(times,prevalent,paste0("time",rep(1:12)))
+  rm(df2)
+
+ # # "AB_date_count"1-10
+ # df3=df%>%select(patient_id,age,sex,ab_count_10)
+ # colnames(df3)[4:15]=paste0("time",rep(1:10))
+  #df3.1=df3%>%gather(times,count,paste0("time",rep(1:10)))
+  #rm(df3)
+
+  # ab_category 1-10
+  df3=df%>%select(patient_id,age,sex,ab_category)
+  colnames(df3)[4:15]=paste0("time",rep(1:12))
+  df3.1=df3%>%gather(times,infection,paste0("time",rep(1:12)))
+  rm(df3)
+  
+  # merge
+  DF=merge(df1.1,df2.1,by=c("patient_id","age","sex","times"))
+  DF=merge(DF,df3.1,by=c("patient_id","age","sex","times"))
+  #DF=merge(DF,df4.1,by=c("patient_id","age","sex","times"))
+  
+  # exclude observation without AB prescription date
+  DF=DF%>%filter(!is.na(date))
+  DF$date=as.Date(DF$date,origin="1970-01-01")
+  
+
+ 
+  temp[[i]] <- DF
+  rm(DF,df1.1,df2.1,df3.1)
   
 }
+#write_rds(temp, "recorded_ab_2019.rds")
+write_rds(temp, here::here("output","measures", "recorded_ab_2019.rds")))
+rm(df,temp)
+
+
+
+
+
+
+
+
+# transform dataset & create list 2020
+temp <- vector("list", length(csvFiles_20))
+
+for (i in seq_along(csvFiles_20)){
+
+  # read in one-month data
+  df <- read_csv(
+    here::here("output", "measures", csvFiles_20[i]))
+  
+  # filter all antibiotics users
+  df=df%>%filter(antibacterial_brit !=0)
+  
+  # create date column
+  df$date=as.Date(date_20[i])
+  
+  
+  #### patient/row --> prescription/row
+  
+  # ab_date_1-12
+  df1=df%>%select(patient_id,age,sex,ab_date_12)
+  colnames(df1)[4:15]=paste0("time",rep(1:12))
+  df1.1=df1%>%gather(times,date,paste0("time",rep(1:12)))
+  rm(df1)
+
+  # prevalent_AB_date_1-10
+  df2=df%>%select(patient_id,age,sex,prevalent_check)
+  colnames(df2)[4:15]=paste0("time",rep(1:12))
+  df2.1=df2%>%gather(times,prevalent,paste0("time",rep(1:12)))
+  rm(df2)
+
+ # # "AB_date_count"1-10
+ # df3=df%>%select(patient_id,age,sex,ab_count_10)
+ # colnames(df3)[4:15]=paste0("time",rep(1:10))
+  #df3.1=df3%>%gather(times,count,paste0("time",rep(1:10)))
+  #rm(df3)
+
+  # ab_category 1-10
+  df3=df%>%select(patient_id,age,sex,ab_category)
+  colnames(df3)[4:15]=paste0("time",rep(1:12))
+  df3.1=df3%>%gather(times,infection,paste0("time",rep(1:12)))
+  rm(df3)
+  
+  # merge
+  DF=merge(df1.1,df2.1,by=c("patient_id","age","sex","times"))
+  DF=merge(DF,df3.1,by=c("patient_id","age","sex","times"))
+  #DF=merge(DF,df4.1,by=c("patient_id","age","sex","times"))
+  
+  # exclude observation without AB prescription date
+  DF=DF%>%filter(!is.na(date))
+  DF$date=as.Date(DF$date,origin="1970-01-01")
+  
+
+ 
+  temp[[i]] <- DF
+  rm(DF,df1.1,df2.1,df3.1)
+  
+}
+#write_rds(temp, "recorded_ab_2020.rds")
+write_rds(temp, here::here("output","measures", "recorded_ab_2020.rds")))
+rm(df,temp)
+
+
+
+# transform dataset & create list 2021
+temp <- vector("list", length(csvFiles_21))
+
+for (i in seq_along(csvFiles_21)){
+
+  # read in one-month data
+  df <- read_csv(
+    here::here("output", "measures", csvFiles_21[i]))
+  
+  # filter all antibiotics users
+  df=df%>%filter(antibacterial_brit !=0)
+  
+  # create date column
+  df$date=as.Date(date_21[i])
+  
+  
+  #### patient/row --> prescription/row
+  
+  # ab_date_1-12
+  df1=df%>%select(patient_id,age,sex,ab_date_12)
+  colnames(df1)[4:15]=paste0("time",rep(1:12))
+  df1.1=df1%>%gather(times,date,paste0("time",rep(1:12)))
+  rm(df1)
+
+  # prevalent_AB_date_1-10
+  df2=df%>%select(patient_id,age,sex,prevalent_check)
+  colnames(df2)[4:15]=paste0("time",rep(1:12))
+  df2.1=df2%>%gather(times,prevalent,paste0("time",rep(1:12)))
+  rm(df2)
+
+ # # "AB_date_count"1-10
+ # df3=df%>%select(patient_id,age,sex,ab_count_10)
+ # colnames(df3)[4:15]=paste0("time",rep(1:10))
+  #df3.1=df3%>%gather(times,count,paste0("time",rep(1:10)))
+  #rm(df3)
+
+  # ab_category 1-10
+  df3=df%>%select(patient_id,age,sex,ab_category)
+  colnames(df3)[4:15]=paste0("time",rep(1:12))
+  df3.1=df3%>%gather(times,infection,paste0("time",rep(1:12)))
+  rm(df3)
+  
+  # merge
+  DF=merge(df1.1,df2.1,by=c("patient_id","age","sex","times"))
+  DF=merge(DF,df3.1,by=c("patient_id","age","sex","times"))
+  #DF=merge(DF,df4.1,by=c("patient_id","age","sex","times"))
+  
+  # exclude observation without AB prescription date
+  DF=DF%>%filter(!is.na(date))
+  DF$date=as.Date(DF$date,origin="1970-01-01")
+  
+
+ 
+  temp[[i]] <- DF
+  rm(DF,df1.1,df2.1,df3.1)
+  
+}
+#write_rds(temp, "recorded_ab_2021.rds")
+write_rds(temp, here::here("output","measures", "recorded_ab_2021.rds")))
+rm(df,temp)
+
+
+
+
+# transform dataset & create list 2022
+temp <- vector("list", length(csvFiles_22))
+
+for (i in seq_along(csvFiles_22)){
+
+  # read in one-month data
+  df <- read_csv(
+    here::here("output", "measures", csvFiles_22[i]))
+  
+  # filter all antibiotics users
+  df=df%>%filter(antibacterial_brit !=0)
+  
+  # create date column
+  df$date=as.Date(date_22[i])
+  
+  
+  #### patient/row --> prescription/row
+  
+  # ab_date_1-12
+  df1=df%>%select(patient_id,age,sex,ab_date_12)
+  colnames(df1)[4:15]=paste0("time",rep(1:12))
+  df1.1=df1%>%gather(times,date,paste0("time",rep(1:12)))
+  rm(df1)
+
+  # prevalent_AB_date_1-10
+  df2=df%>%select(patient_id,age,sex,prevalent_check)
+  colnames(df2)[4:15]=paste0("time",rep(1:12))
+  df2.1=df2%>%gather(times,prevalent,paste0("time",rep(1:12)))
+  rm(df2)
+
+ # # "AB_date_count"1-10
+ # df3=df%>%select(patient_id,age,sex,ab_count_10)
+ # colnames(df3)[4:15]=paste0("time",rep(1:10))
+  #df3.1=df3%>%gather(times,count,paste0("time",rep(1:10)))
+  #rm(df3)
+
+  # ab_category 1-10
+  df3=df%>%select(patient_id,age,sex,ab_category)
+  colnames(df3)[4:15]=paste0("time",rep(1:12))
+  df3.1=df3%>%gather(times,infection,paste0("time",rep(1:12)))
+  rm(df3)
+  
+  # merge
+  DF=merge(df1.1,df2.1,by=c("patient_id","age","sex","times"))
+  DF=merge(DF,df3.1,by=c("patient_id","age","sex","times"))
+  #DF=merge(DF,df4.1,by=c("patient_id","age","sex","times"))
+  
+  # exclude observation without AB prescription date
+  DF=DF%>%filter(!is.na(date))
+  DF$date=as.Date(DF$date,origin="1970-01-01")
+  
+
+ 
+  temp[[i]] <- DF
+  rm(DF,df1.1,df2.1,df3.1)
+  
+}
+#write_rds(temp, "recorded_ab_2022.rds")
+write_rds(temp, here::here("output","measures", "recorded_ab_2022.rds")))
+rm(df,temp)
+
+
+
