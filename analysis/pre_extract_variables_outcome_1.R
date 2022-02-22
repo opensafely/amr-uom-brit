e546bc243512592c55581180352ce17452f31b4c
rriefu <84485899+rriefu@users.noreply.github.com>
2022-02-22 00:54:46 +0000

    group cases_outcome1

--- analysis/pre_extract_variables_outcome_1.R
+++ analysis/pre_extract_variables_outcome_1.R
@@ -4,6 +4,7 @@
 
 library('tidyverse')
 library("ggplot2")
+library('plyr')
 library('dplyr')
 library('lubridate')
 
@@ -19,20 +20,37 @@ filename=c("2003","2004","2005","2006","2007","2008","2009","2010","2011","2012"
 datelist=seq(as.Date("2020-03-01"), by = "month", length.out = 22)
 
 
-
+#  monthly controls
 for (i in 1:length(filename)){
 
 df=read_csv(paste0("matched_combined_general_population_infection_",filename[i],".csv"))
 
-case= df%>% filter(case==1)
 
-control= df%>% filter(case==0)
-control$patient_index_date=as.Date(datelist[i])
-
-
-write_csv(case, here::here("output", paste0("matched_outcome_1_case_",filename[i],".csv")))
+control= df%>% dplyr::filter(case==0)
+control$patient_index_date=as.Date(datelist[i]) # original index date
 
 write_csv(control, here::here("output", paste0("matched_outcome_1_control_",filename[i],".csv")))
 
-rm(df,case,control)
+rm(df,control)
 }
+
+
+
+# all cases
+
+temp <- vector("list", length(filename))
+
+for (i in 1:length(filename)){
+  
+  df=read_csv(paste0("matched_combined_general_population_infection_",filename[i],".csv"))
+  
+  case= df%>% dplyr::filter(case==1)
+  
+  temp[[i]] =case
+  
+  rm(df,case)
+}
+
+case=plyr::ldply(temp, data.frame)
+
+write_csv(case, here::here("output", "matched_outcome_1_case.csv"))
