2e744f34cc44b87f34e60dddd8e4c7544624f81e
rriefu <84485899+rriefu@users.noreply.github.com>
2022-02-23 20:42:18 +0000

    split case & control

--- analysis/plot/infection_abtype.R
+++ analysis/plot/infection_abtype.R
@@ -5,6 +5,7 @@ library("dplyr")
 library('here')
 library("tidyverse")
 library("lubridate")
+library("ggpubr")
 
 rm(list=ls())
 setwd(here::here("output", "measures"))
@@ -26,62 +27,117 @@ last_mon <- (format(max(df$date), "%m-%Y"))
 # list size per month
 df=df%>%group_by(date)%>%
   mutate(patient=sum(length(unique(df$patient_id))))
-  
 
-# abtype counts
-df=df%>%group_by(date,abtype,prevalent)%>%
-  summarise(count=n(),
-            patient=mean(patient))
+# variable types
+df$prevalent=as.factor(df$prevalent)
+df$date=as.Date(df$date)
+df$abtype=as.character(df$abtype)
 
 
-# calculate rate
-df$value=df$count/df$patient
+# select prevalent cases
+df.1=df%>%filter(prevalent==1)%>%group_by(date,abtype)%>%summarise(count=n(),patient=mean(patient))
+df.1$value=df.1$count/df.1$patient*1000
 
-### select most common ab###
-DF.top10=df%>%
+#top 10 ab
+DF.top10.1=df.1%>%
   group_by(abtype)%>%
-  summarise(value=mean(value))%>% # RX: average per month
-  arrange(desc(value))%>%
+  summarise(value2=mean(value))%>% # RX: average per month
+  arrange(desc(value2))%>%
   slice(1:10)
 
+# sort ab type
+# recode other types
+df.1$type=ifelse(df.1$abtype %in% DF.top10.1$abtype | is.na(df.1$abtype), df.1$abtype, "Others")
 
- # sort ab type
-         
-        # recode other types
-         df$type=ifelse(df$abtype %in% DF.top10$abtype | is.na(df$abtype), df$abtype, "Others")
-         
-         # recode NA -> no recorded antibiotics
-         df$type=ifelse( is.na(df$type),"No Antibiotics", df$type)
-         
-         df$type <- factor(df$type, levels=c(DF.top10$abtype,"Others","No Antibiotics"))# reorder
-         
-         # summarise data
-         df.plot=df%>%group_by(type,date,prevalent)%>%
-           summarise(
-             value2=sum(value)
-           )
+# recode NA -> no recorded antibiotics
+df.1$type=ifelse(is.na(df.1$type),"No Antibiotics", df.1$type)
+df.1$type <- factor(df.1$type, levels=c(DF.top10.1$abtype,"Others","No Antibiotics"))# reorder
 
+# summarise data
+df.plot.1=df.1%>%group_by(type,date)%>%
+  summarise(
+    value2=sum(value)
+  )
+
+
+# select incident cases
+df.0=df%>%filter(prevalent==0)%>%group_by(date,abtype)%>%summarise(count=n(),patient=mean(patient))
+df.0$value=df.0$count/df.0$patient*1000
+
+#top 10 ab
+DF.top10.0=df.0%>%
+  group_by(abtype)%>%
+  summarise(value2=mean(value))%>% # RX: average per month
+  arrange(desc(value2))%>%
+  slice(1:10)
+
+# sort ab type
+# recode other types
+df.0$type=ifelse(df.0$abtype %in% DF.top10.0$abtype | is.na(df.0$abtype), df.0$abtype, "Others")
+
+# recode NA -> no recorded antibiotics
+df.0$type=ifelse(is.na(df.0$type),"No Antibiotics", df.0$type)
+df.0$type <- factor(df.0$type, levels=c(DF.top10.0$abtype,"Others","No Antibiotics"))# reorder
+
+
+# summarise data
+df.plot.0=df.0%>%group_by(type,date)%>%
+  summarise(
+    value2=sum(value)
+  )
+
+df.1$prevalent=as.factor(1)
+df.0$prevalent=as.factor(0)
+df=rbind(df.0,df.1)
 
 ## # line graph-rate
-lineplot<- ggplot(df.plot, aes(x=date, y=value2,group=type,color=type))+
+# prevalent
+lineplot.1<- ggplot(df.plot.1, aes(x=date, y=value2,group=type,color=type))+
   annotate(geom = "rect", xmin = as.Date("2021-01-01"),xmax = as.Date("2021-04-01"),ymin = -Inf, ymax = Inf,fill="grey80", alpha=0.5)+
   annotate(geom = "rect", xmin = as.Date("2020-11-01"),xmax = as.Date("2020-12-01"),ymin = -Inf, ymax = Inf,fill="grey80", alpha=0.5)+
   annotate(geom = "rect", xmin = as.Date("2020-03-01"),xmax = as.Date("2020-06-01"),ymin = -Inf, ymax = Inf,fill="grey80", alpha=0.5)+
-  facet_grid(rows = vars(prevalent))+
-  geom_line()+
-  theme(legend.position = "bottom",legend.title =element_blank())+
+  geom_line(aes(linetype=type))+
+  geom_point(aes(shape=type))+
+  theme(legend.position = "right",legend.title =element_blank())+
+  scale_shape_manual(values = c(rep(1:12))) +
+  scale_color_manual(values = c("coral2","deeppink3","darkred","darkviolet","brown3","goldenrod2","blue3","green3","forestgreen","dodgerblue","deepskyblue","azure4"))+
   labs(
-    fill = "Antibiotic type",
-    title = "Top 10 Antibiotic Types Prescribed for UTI patients",
-    subtitle = paste(first_mon,"-",last_mon),
-    caption = "Grey shading represents national lockdown time.
-    0 means incident infection; 1 means prevalent infection.",
-    y = "Number of prescriptions per 1000 UTI patients",
+    y = "",
     x=""
   )+
   theme(axis.text.x=element_text(angle=60,hjust=1))+
   scale_x_date(date_labels = "%m-%Y", date_breaks = "1 month")+
   scale_y_continuous(n.breaks = 10)
+
+# incident
+lineplot.0<- ggplot(df.plot.0, aes(x=date, y=value2,group=type,color=type))+
+  annotate(geom = "rect", xmin = as.Date("2021-01-01"),xmax = as.Date("2021-04-01"),ymin = -Inf, ymax = Inf,fill="grey80", alpha=0.5)+
+  annotate(geom = "rect", xmin = as.Date("2020-11-01"),xmax = as.Date("2020-12-01"),ymin = -Inf, ymax = Inf,fill="grey80", alpha=0.5)+
+  annotate(geom = "rect", xmin = as.Date("2020-03-01"),xmax = as.Date("2020-06-01"),ymin = -Inf, ymax = Inf,fill="grey80", alpha=0.5)+
+  geom_line(aes(linetype=type))+
+  geom_point(aes(shape=type))+
+  theme(legend.position = "right",legend.title =element_blank())+
+  scale_shape_manual(values = c(rep(1:12))) +
+  scale_color_manual(values = c("coral2","deeppink3","darkred","darkviolet","brown3","goldenrod2","blue3","green3","forestgreen","dodgerblue","deepskyblue","azure4"))+
+  labs(
+    y = "" ,
+    x="")+
+  theme(axis.text.x=element_text(angle=60,hjust=1))+
+  scale_x_date(date_labels = "%m-%Y", date_breaks = "1 month")+
+  scale_y_continuous(n.breaks = 10)
+
+lineplot=ggarrange(lineplot.0, lineplot.1, 
+          labels = c("A", "B"),
+          nrow = 2)
+
+annotate_figure(lineplot,
+                top = text_grob("Top 10 Antibiotic Types Prescribed for UTI patients", face = "bold", size = 18),
+                bottom = text_grob("A= incident cases; B= prevalent cases.
+                                   Grey shading represents national lockdown time.", 
+                                   hjust = 1, x = 1, size = 10),
+                left = text_grob("Number of prescriptions per 1000 UTI patients", rot = 90),
+)
+
 
 ggsave(
   plot= lineplot,
