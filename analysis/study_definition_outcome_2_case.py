a25295d965c29324445711bc511675bdba3c70ef
rriefu <rriefu@gmail.com>
2022-04-06 15:31:55 +0100

    study definition : change index date to patient_index_date

--- analysis/study_definition_outcome_2_case.py
+++ analysis/study_definition_outcome_2_case.py
@@ -68,7 +68,7 @@ study = StudyDefinition(
 
     ## Age
     age=patients.age_as_of(
-        "index_date",
+        "patient_index_date",
         return_expectations={
             "rate": "universal",
             "int": {"distribution": "population_ages"},
