ed67e795be6254c1c8657a605abacf6274a7f56d
rriefu <rriefu@gmail.com>
2022-04-09 09:35:56 +0100

    add "patient_index_date"

--- analysis/process_Rmatching.R
+++ analysis/process_Rmatching.R
@@ -172,7 +172,7 @@ df$covrx_ever=ifelse(df$covrx1_ever>0|df$covrx2_ever>0,1,0)
 
 
 # variables for analysis
-df=subset(df,select=c("wave","patient_id","subclass","case","sex","age","age_cat","stp","region","ethnicity_6","bmi","bmi_cat","CCI","Charlson","smoking_cat_3","imd","care_home","covrx_ever","flu_vaccine", "ab_prescriptions","broad_ab_prescriptions","ab_types","interval", "lastABtime","ab_qn", "br_ab_qn"))
+df=subset(df,select=c("wave","patient_index_date","patient_id","subclass","case","sex","age","age_cat","stp","region","ethnicity_6","bmi","bmi_cat","CCI","Charlson","smoking_cat_3","imd","care_home","covrx_ever","flu_vaccine", "ab_prescriptions","broad_ab_prescriptions","ab_types","interval", "lastABtime","ab_qn", "br_ab_qn"))
 
 
 
