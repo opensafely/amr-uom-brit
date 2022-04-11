8340157b0d90e04d26391a178f3b592a9653e7e9
BillyZhongUOM <61462861+BillyZhongUOM@users.noreply.github.com>
2022-04-11 23:34:34 +0100

    remove 2022

--- analysis/process/ab_type_transform_2.R
+++ analysis/process/ab_type_transform_2.R
@@ -107,85 +107,85 @@ for (i in seq_along(csvFiles_21)){
 saveRDS(temp, "ab_type_2021.rds")
 rm(temp)
 
-## save 2022 record
-temp <- vector("list", length(csvFiles_22))
-for (i in seq_along(csvFiles_22)){
-  # read in one-month data
-  df <- read_csv((csvFiles_22[i]),
-                 col_types = cols_only(
-                   AB_date_1 = col_date(format = ""),
-                   AB_date_2 = col_date(format = ""),
-                   AB_date_3 = col_date(format = ""),
-                   AB_date_4 = col_date(format = ""),
-                   AB_date_5 = col_date(format = ""),
-                   AB_date_6 = col_date(format = ""),
-                   AB_date_7 = col_date(format = ""),
-                   AB_date_8 = col_date(format = ""),
-                   AB_date_9 = col_date(format = ""),
-                   AB_date_10 = col_date(format = ""),
-                   AB_date_11 = col_date(format = ""),
-                   AB_date_12 = col_date(format = ""),
-                   age = col_integer(),
-                   age_cat = col_character(),
-                   sex = col_character(),
-                   practice = col_integer(),
-                   antibacterial_brit = col_integer(),
-                   AB_date_1_indication = col_character(),
-                   AB_date_2_indication = col_character(),
-                   AB_date_3_indication = col_character(),
-                   AB_date_4_indication = col_character(),
-                   AB_date_5_indication = col_character(),
-                   AB_date_6_indication = col_character(),
-                   AB_date_7_indication = col_character(),
-                   AB_date_8_indication = col_character(),
-                   AB_date_9_indication = col_character(),
-                   AB_date_10_indication = col_character(),
-                   AB_date_11_indication = col_character(),
-                   AB_date_12_indication = col_character(),
-                   Ab_date_1_type = col_character(),
-                   Ab_date_2_type = col_character(),
-                   Ab_date_3_type = col_character(),
-                   Ab_date_4_type = col_character(),
-                   Ab_date_5_type = col_character(),
-                   Ab_date_6_type = col_character(),
-                   Ab_date_7_type = col_character(),
-                   Ab_date_8_type = col_character(),
-                   Ab_date_9_type = col_character(),
-                   Ab_date_10_type = col_character(),
-                   Ab_date_11_type = col_character(),
-                   Ab_date_12_type = col_character(),
-                   patient_id = col_integer()
-                 ),
-                 na = character())
+# ## save 2022 record
+# temp <- vector("list", length(csvFiles_22))
+# for (i in seq_along(csvFiles_22)){
+#   # read in one-month data
+#   df <- read_csv((csvFiles_22[i]),
+#                  col_types = cols_only(
+#                    AB_date_1 = col_date(format = ""),
+#                    AB_date_2 = col_date(format = ""),
+#                    AB_date_3 = col_date(format = ""),
+#                    AB_date_4 = col_date(format = ""),
+#                    AB_date_5 = col_date(format = ""),
+#                    AB_date_6 = col_date(format = ""),
+#                    AB_date_7 = col_date(format = ""),
+#                    AB_date_8 = col_date(format = ""),
+#                    AB_date_9 = col_date(format = ""),
+#                    AB_date_10 = col_date(format = ""),
+#                    AB_date_11 = col_date(format = ""),
+#                    AB_date_12 = col_date(format = ""),
+#                    age = col_integer(),
+#                    age_cat = col_character(),
+#                    sex = col_character(),
+#                    practice = col_integer(),
+#                    antibacterial_brit = col_integer(),
+#                    AB_date_1_indication = col_character(),
+#                    AB_date_2_indication = col_character(),
+#                    AB_date_3_indication = col_character(),
+#                    AB_date_4_indication = col_character(),
+#                    AB_date_5_indication = col_character(),
+#                    AB_date_6_indication = col_character(),
+#                    AB_date_7_indication = col_character(),
+#                    AB_date_8_indication = col_character(),
+#                    AB_date_9_indication = col_character(),
+#                    AB_date_10_indication = col_character(),
+#                    AB_date_11_indication = col_character(),
+#                    AB_date_12_indication = col_character(),
+#                    Ab_date_1_type = col_character(),
+#                    Ab_date_2_type = col_character(),
+#                    Ab_date_3_type = col_character(),
+#                    Ab_date_4_type = col_character(),
+#                    Ab_date_5_type = col_character(),
+#                    Ab_date_6_type = col_character(),
+#                    Ab_date_7_type = col_character(),
+#                    Ab_date_8_type = col_character(),
+#                    Ab_date_9_type = col_character(),
+#                    Ab_date_10_type = col_character(),
+#                    Ab_date_11_type = col_character(),
+#                    Ab_date_12_type = col_character(),
+#                    patient_id = col_integer()
+#                  ),
+#                  na = character())
   
-  # filter all antibiotics users
-  df=df%>%filter(antibacterial_brit !=0)
+#   # filter all antibiotics users
+#   df=df%>%filter(antibacterial_brit !=0)
   
-  df1=df%>%select(patient_id,age,sex,ab_date_12)
-  colnames(df1)[4:15]=paste0("time",rep(1:12))
-  df1.1=df1%>%gather(times,date,paste0("time",rep(1:12)))
-  rm(df1)
+#   df1=df%>%select(patient_id,age,sex,ab_date_12)
+#   colnames(df1)[4:15]=paste0("time",rep(1:12))
+#   df1.1=df1%>%gather(times,date,paste0("time",rep(1:12)))
+#   rm(df1)
   
-  df2=df%>%select(patient_id,age,sex,all_of(ab_type))
-  colnames(df2)[4:15]=paste0("time",rep(1:12))
-  df2.1=df2%>%gather(times,type,paste0("time",rep(1:12)))
-  rm(df2)
+#   df2=df%>%select(patient_id,age,sex,all_of(ab_type))
+#   colnames(df2)[4:15]=paste0("time",rep(1:12))
+#   df2.1=df2%>%gather(times,type,paste0("time",rep(1:12)))
+#   rm(df2)
   
-  df3=df%>%select(patient_id,age,sex,all_of(ab_category))
-  colnames(df3)[4:15]=paste0("time",rep(1:12))
-  df3.1=df3%>%gather(times,infection,paste0("time",rep(1:12)))
-  rm(df3)
+#   df3=df%>%select(patient_id,age,sex,all_of(ab_category))
+#   colnames(df3)[4:15]=paste0("time",rep(1:12))
+#   df3.1=df3%>%gather(times,infection,paste0("time",rep(1:12)))
+#   rm(df3)
   
-  DF=merge(df1.1,df2.1,by=c("patient_id","age","sex","times"))
-  DF=merge(DF,df3.1,by=c("patient_id","age","sex","times"))
+#   DF=merge(df1.1,df2.1,by=c("patient_id","age","sex","times"))
+#   DF=merge(DF,df3.1,by=c("patient_id","age","sex","times"))
   
-  DF=DF%>%filter(!is.na(date))
-  DF$Date=date_22[i]
+#   DF=DF%>%filter(!is.na(date))
+#   DF$Date=date_22[i]
   
-  temp[[i]] <- DF
-  rm(DF,df1.1,df2.1,df3.1)
+#   temp[[i]] <- DF
+#   rm(DF,df1.1,df2.1,df3.1)
   
-}
+# }
 
-saveRDS(temp, "ab_type_2022.rds")
-rm(temp)
+# saveRDS(temp, "ab_type_2022.rds")
+# rm(temp)
