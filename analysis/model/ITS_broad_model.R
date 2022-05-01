332a96de98ad6b6b472affca0195c7d09e7c25cc
BillyZhongUOM <61462861+BillyZhongUOM@users.noreply.github.com>
2022-05-01 15:14:30 +0100

    Update ITS_broad_model.R

--- analysis/model/ITS_broad_model.R
+++ analysis/model/ITS_broad_model.R
@@ -189,7 +189,6 @@ its_function <- function(outcomes_vec = outcomes,
           panel.grid.minor.x = element_blank(),
           panel.grid.minor.y = element_line(size=.2, color=rgb(0,0,0,0.2)) ,
           panel.grid.major.y = element_line(size=.2, color=rgb(0,0,0,0.3)))+
-		scale_x_date(breaks = "1 year", date_labels = "%Y") +
 		labs(x = "Year")
 
   plot1
