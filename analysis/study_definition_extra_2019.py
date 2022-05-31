f7ed33e32de524c793da68f83d0c74ac2935a4f9
BillyZhongUOM <61462861+BillyZhongUOM@users.noreply.github.com>
2022-05-31 15:04:47 +0100

    update

---
+++ analysis/study_definition_extra_2019.py
@@ -0,0 +1,207 @@
+
+
+from cohortextractor import (
+    StudyDefinition,
+    patients,
+    #codelist_from_csv,
+    codelist,
+    filter_codes_by_category,
+    #combine_codelists,
+    Measure
+)
+
+###### import matched cohort
+COHORT = "output/ab_id_date_2019.csv"
+
+###### Code lists
+from codelists import *
+
+###### Define study time variables
+from datetime import datetime
+start_date = "2019-01-01"
+end_date = "2019-12-31"
+
+####### Import variables
+
+# ## infection before patient_index_date
+# from variables_infection import generate_infection_variables
+# infection_variables = generate_infection_variables(index_date_variable="patient_index_date")
+
+
+# ## Demographics, vaccine, included as they are potential confounders 
+# from variables_confounding import generate_confounding_variables
+# confounding_variables = generate_confounding_variables(index_date_variable="patient_index_date")
+
+# ## Charlson Comobidity Index
+# from variables_CCI import generate_CCI_variables
+# CCI_variables = generate_CCI_variables(index_date_variable="patient_index_date")
+
+
+study = StudyDefinition(
+
+    # Configure the expectations framework
+    default_expectations={
+        "date": {"earliest": start_date, "latest": end_date},
+        "rate": "uniform",
+        "incidence": 0.1,
+    },
+
+    # Set index date to start date
+    index_date=start_date,
+   
+    # study population
+    population=patients.which_exist_in_file(COHORT),
+
+    ### patient index date  
+    # case_infection_date
+    patient_index_date=patients.with_value_from_file(
+        COHORT,
+        returning="patient_index_date",
+        returning_type="date",
+    ),
+
+    ## Age
+    age=patients.age_as_of(
+        "patient_index_date",
+        return_expectations={
+            "rate": "universal",
+            "int": {"distribution": "population_ages"},
+            "incidence": 0.001
+        },
+    ),
+
+    
+    ### Sex
+    sex=patients.sex(
+        return_expectations={
+            "rate": "universal",
+            "category": {"ratios": {"M": 0.49, "F": 0.51}},
+        }
+    ),
+
+## region
+    stp=patients.registered_practice_as_of(
+             "patient_index_date",
+            returning="stp_code",
+            return_expectations={
+                "rate": "universal",
+                "category": {
+                    "ratios": {
+                        "STP1": 0.1,
+                        "STP2": 0.1,
+                        "STP3": 0.1,
+                        "STP4": 0.1,
+                        "STP5": 0.1,
+                        "STP6": 0.1,
+                        "STP7": 0.1,
+                        "STP8": 0.1,
+                        "STP9": 0.1,
+                        "STP10": 0.1,
+                    }
+                },
+            },
+    ),
+
+    # self-reported ethnicity 
+    ethnicity=patients.with_these_clinical_events(
+        ethnicity_codes,
+        returning="category",
+        find_last_match_in_period=True,
+        include_date_of_match=False,
+        return_expectations={
+            "category": {"ratios": {"1": 0.8, "5": 0.1, "3": 0.1}},
+            "incidence": 0.75,
+        },
+    ),
+
+   # ever hospitalisation
+    hospital_counts=patients.admitted_to_hospital(
+        returning= "number_of_matches_in_period" ,  
+        between=['patient_index_date - 366 days', "patient_index_date - 1 day"],
+        return_expectations={"int": {"distribution": "normal",
+                                     "mean": 25, "stddev": 5}, "incidence": 1}
+   ),
+
+
+    # index of multiple deprivation, estimate of SES based on patient post code 
+	imd=patients.categorised_as(
+        {
+            "0": "DEFAULT",
+            "1": """index_of_multiple_deprivation >=1 AND index_of_multiple_deprivation < 32844*1/5""",
+            "2": """index_of_multiple_deprivation >= 32844*1/5 AND index_of_multiple_deprivation < 32844*2/5""",
+            "3": """index_of_multiple_deprivation >= 32844*2/5 AND index_of_multiple_deprivation < 32844*3/5""",
+            "4": """index_of_multiple_deprivation >= 32844*3/5 AND index_of_multiple_deprivation < 32844*4/5""",
+            "5": """index_of_multiple_deprivation >= 32844*4/5 AND index_of_multiple_deprivation < 32844""",
+        },
+        index_of_multiple_deprivation=patients.address_as_of(
+            "patient_index_date",
+            returning="index_of_multiple_deprivation",
+            round_to_nearest=100,
+        ),
+        return_expectations={
+            "rate": "universal",
+            "category": {
+                "ratios": {
+                    "0": 0.05,
+                    "1": 0.19,
+                    "2": 0.19,
+                    "3": 0.19,
+                    "4": 0.19,
+                    "5": 0.19,
+                }
+            },
+        },
+    ),   
+    # LIFESTYLE
+    ## BMI, most recent
+    bmi=patients.most_recent_bmi(
+        between=['patient_index_date- 5 years', 'patient_index_date'],
+        minimum_age_at_measurement=18,
+        include_measurement_date=True,
+        date_format="YYYY-MM-DD",
+        return_expectations={
+            "date": {"earliest": "2010-02-01", "latest": "today"},
+            "float": {"distribution": "normal", "mean": 28, "stddev": 8},
+            "incidence": 0.80,
+        },
+    ),
+
+    #smoking https://github.com/ebmdatalab/tpp-sql-notebook/issues/6
+    smoking_status=patients.categorised_as(
+        {
+            "S": "most_recent_smoking_code = 'S'",
+            "E": """
+                 most_recent_smoking_code = 'E' OR (
+                   most_recent_smoking_code = 'N' AND ever_smoked
+                 )
+            """,
+            "N": "most_recent_smoking_code = 'N' AND NOT ever_smoked",
+            "M": "DEFAULT",
+        },
+        return_expectations={
+            "category": {"ratios": {"S": 0.6, "E": 0.1, "N": 0.2, "M": 0.1}}
+        },
+        most_recent_smoking_code=patients.with_these_clinical_events(
+            clear_smoking_codes,
+            find_last_match_in_period=True,
+            on_or_before="patient_index_date",
+            returning="category",
+        ),
+        ever_smoked=patients.with_these_clinical_events(
+            filter_codes_by_category(clear_smoking_codes, include=["S", "E"]),
+            on_or_before="patient_index_date",
+        ),
+    ),
+    smoking_status_date=patients.with_these_clinical_events(
+        clear_smoking_codes,
+        on_or_before="patient_index_date",
+        return_last_date_in_period=True,
+        include_month=True,
+    ),
+    most_recent_unclear_smoking_cat_date=patients.with_these_clinical_events(
+        unclear_smoking_codes,
+        on_or_before="patient_index_date",
+        return_last_date_in_period=True,
+        include_month=True,
+    ),
+)
