e06968bc9fc2f3d264bd83da0a9fa48664dddc1f
BillyZhongUOM <61462861+BillyZhongUOM@users.noreply.github.com>
2022-06-01 11:23:38 +0100

    update

---
+++ analysis/description/figure.R
@@ -0,0 +1,155 @@
+### This script is for preparing the variables for repeat antibiotics model ###
+library("dplyr")
+library("tidyverse")
+library("lubridate")
+library('stringr')
+library("data.table")
+library("ggpubr")
+library("finalfit")
+
+rm(list=ls())
+setwd(here::here("output", "measures"))
+
+DF <- readRDS("cleaned_indication_ab.rds")
+
+# recode
+DF$date <- as.Date(DF$date)
+DF <- DF %>% filter(date <= as.Date("2021-12-31"))
+DF <- DF %>% filter(age > 3)
+setwd(here::here("output"))
+df2.1 <- read_csv("prepared_var_2019.csv") %>% select(-c(age,sex))
+df2.2 <- read_csv("prepared_var_2020.csv") %>% select(-c(age,sex))
+df2.3 <- read_csv("prepared_var_2021.csv") %>% select(-c(age,sex))
+df2 <- rbind(df2.1,df2.2,df2.3)
+df <- merge(DF,df2,by=c("patient_id"))
+rm(DF,df2)
+
+df$ethnicity_6 <- as.factor(df$ethnicity_6)
+df$imd <- as.factor(df$imd)
+df$region <- as.factor(df$region)
+df$charlsonGrp <- as.factor(df$charlsonGrp)
+df$ab12b4 <- as.factor(df$ab12b4)
+df <- df %>% dplyr::select(patient_id,incidental,infection,type,repeat_ab,age,sex,ethnicity_6,region,charlsonGrp,imd,ab12b4,date)
+df <- df %>% filter (df$sex=="M"|df$sex=="F")
+df <- df %>% mutate(age_group = case_when(age>3 & age<=15 ~ "<16",
+                                          age>=16 & age<=44 ~ "16-44",
+                                          age>=45 & age<=64 ~ "45-64",
+                                          age>=65 ~ "65+"))
+
+
+### prepare var ### without Amoxicillin ###
+broadtype <- c("Ampicillin","Co-amoxiclav","Moxifloxacin","Cefaclor","Cefadroxil",
+               "Cefuroxime", "Cefalexin","Cefazolin","Cefixime","Cefotaxime","Cefoxitin","Cefradine",
+               "Cefpirome","Ceftazidime","Ceftriaxone", "Cefprozil","Ciprofloxacin","Co-fluampicil",
+               "Doripenem","Ertapenem", "Cilastatin","Cefamandole","Levofloxacin" , 
+               "Meropenem" ,"Nalidixic acid","Norfloxacin", "Ofloxacin","Cefpodoxime","Cefepime")
+
+start_covid = as.Date("2020-04-01")
+covid_adjustment_period_from = as.Date("2020-03-01")
+
+###  Prepare the data frame for Interrupted time-series analysis  ###
+###  Transfer df into numOutcome / numEligible  version
+df$cal_year <- year(df$date)
+df$cal_mon <- month(df$date)
+df$time <- as.numeric(df$cal_mon+(df$cal_year-2019)*12)
+DF <- df
+
+### Broad spectrum by age
+
+df.broad <- df %>% filter(type %in% broadtype ) 
+df.broad_total <- df.broad %>% group_by(time,age_group) %>% summarise(
+  numOutcome = n(),
+)
+
+df.all <-  df %>% group_by(time,age_group) %>% summarise(
+  numEligible = n(),
+)
+df.model <- merge(df.broad_total,df.all,by=c("time","age_group"))
+
+df.model <- df.model %>% mutate(mon = case_when(time>=1 & time<=12 ~ time,
+                                                time>=13 & time<=24 ~ time-12,
+                                                time>=24 & time<=36 ~ time-24,
+                                                time>36 ~ time-36)) %>% 
+  mutate(year = case_when(time>=1 & time<=12 ~ 2019,
+                          time>=13 & time<=24 ~ 2020,
+                          time>=24 & time<=36 ~ 2021,
+                          time>36 ~ 2022)) %>% 
+  mutate(day = 1)
+df.model$monPlot <- as.Date(with(df.model,paste(year,mon,day,sep="-")),"%Y-%m-%d")
+
+df.model <- df.model %>% mutate(value = numOutcome/numEligible)
+
+bkg_colour <- "white"
+figure_age_strata <- ggplot(df.model, aes(x = as.Date("2019-01-01"), y = value, group = factor(age_group), col = factor(age_group), fill = factor(age_group))) +
+  geom_boxplot(width=20, outlier.size=0, position="identity", alpha=.5) +
+  geom_line(aes(x = monPlot, y = value), lwd = 1.2)+ 
+  scale_x_date(date_labels = "%b", breaks = "1 year") +
+  geom_vline(xintercept = c(start_covid, 
+                            covid_adjustment_period_from), col = 1, lwd = 1)+
+  labs(x = "Date", y = "% of broad-sepctrum prescription", title = "", colour = "Age", fill = "Age") +
+  theme_classic()  +
+  theme(axis.title = element_text(size = 18),
+        axis.text = element_text(size = 12),
+        axis.text.x = element_text(angle = 60, hjust = 1),
+        legend.text = element_text(size = 12),
+        legend.title = element_text(size = 12),
+        legend.position = "top",
+        strip.background = element_rect(fill = "grey", colour =  NA),
+        strip.text = element_text(size = 12, hjust = 0)) 
+
+figure_age_strata
+
+ggsave(
+  plot= figure_age_strata,
+  filename="figure_age_strata.jpeg", path=here::here("output"),
+)  
+rm(df.broad_total,df.all,df.model)
+
+### Broad spectrum by sex
+
+df.broad_total <- df.broad %>% group_by(time,sex) %>% summarise(
+  numOutcome = n(),
+)
+
+df.all <-  df %>% group_by(time,sex) %>% summarise(
+  numEligible = n(),
+)
+df.model <- merge(df.broad_total,df.all,by=c("time","sex"))
+
+df.model <- df.model %>% mutate(mon = case_when(time>=1 & time<=12 ~ time,
+                                                time>=13 & time<=24 ~ time-12,
+                                                time>=24 & time<=36 ~ time-24,
+                                                time>36 ~ time-36)) %>% 
+  mutate(year = case_when(time>=1 & time<=12 ~ 2019,
+                          time>=13 & time<=24 ~ 2020,
+                          time>=24 & time<=36 ~ 2021,
+                          time>36 ~ 2022)) %>% 
+  mutate(day = 1)
+df.model$monPlot <- as.Date(with(df.model,paste(year,mon,day,sep="-")),"%Y-%m-%d")
+
+df.model <- df.model %>% mutate(value = numOutcome/numEligible)
+
+bkg_colour <- "white"
+figure_sex_strata <- ggplot(df.model, aes(x = as.Date("2019-01-01"), y = value, group = factor(sex), col = factor(sex), fill = factor(sex))) +
+  geom_boxplot(width=20, outlier.size=0, position="identity", alpha=.5) +
+  geom_line(aes(x = monPlot, y = value), lwd = 1.2)+ 
+  scale_x_date(date_labels = "%b", breaks = "1 year") +
+  geom_vline(xintercept = c(start_covid, 
+                            covid_adjustment_period_from), col = 1, lwd = 1)+
+  labs(x = "Date", y = "% of broad-sepctrum prescription", title = "", colour = "Gender", fill = "Gender") +
+  theme_classic()  +
+  theme(axis.title = element_text(size = 18),
+        axis.text = element_text(size = 12),
+        axis.text.x = element_text(angle = 60, hjust = 1),
+        legend.text = element_text(size = 12),
+        legend.title = element_text(size = 12),
+        legend.position = "top",
+        strip.background = element_rect(fill = "grey", colour =  NA),
+        strip.text = element_text(size = 12, hjust = 0)) 
+
+figure_sex_strata
+
+ggsave(
+  plot= figure_sex_strata,
+  filename="figure_sex_strata.jpeg", path=here::here("output"),
+)  
