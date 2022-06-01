a520ec804873639eb97baa4d5eafe8469beeca6c
ali-fahmi <alifahmi1990@gmail.com>
2022-06-01 09:55:31 +0100

    remove sgss_gp_cov_admitted_binary from all measures

--- analysis/study_definition_hospitalisation_rate.py
+++ analysis/study_definition_hospitalisation_rate.py
@@ -256,7 +256,7 @@ measures = [
         id="hosp_rate",
         numerator="admitted",
         denominator="population",
-        group_by=['sgss_gp_cov_admitted_binary'],
+        # group_by=['sgss_gp_cov_admitted_binary'],
         small_number_suppression=True,
     ),
 
@@ -264,7 +264,7 @@ measures = [
         id="hosp_rate_sex",
         numerator="admitted",
         denominator="population",
-        group_by=["sex", 'sgss_gp_cov_admitted_binary'],
+        group_by=["sex"], # , 'sgss_gp_cov_admitted_binary'
         small_number_suppression=True,
     ),
 
@@ -272,7 +272,7 @@ measures = [
         id="hosp_rate_age_cat",
         numerator="admitted",
         denominator="population",
-        group_by=['age_cat', 'sgss_gp_cov_admitted_binary'],
+        group_by=['age_cat'], # , 'sgss_gp_cov_admitted_binary']
         small_number_suppression=True,
     ),
 
@@ -280,7 +280,7 @@ measures = [
         id="hosp_rate_cat",
         numerator="admitted",
         denominator="population",
-        group_by=['admitted_cat', 'sgss_gp_cov_admitted_binary'],
+        group_by=['admitted_cat'], # , 'sgss_gp_cov_admitted_binary']
         small_number_suppression=True,
     ),
     
@@ -288,7 +288,7 @@ measures = [
         id="hosp_rate_gp",
         numerator="admitted",
         denominator="population",
-        group_by=['gp_cons_admitted_binary', 'sgss_gp_cov_admitted_binary'],
+        group_by=['gp_cons_admitted_binary'], #, 'sgss_gp_cov_admitted_binary']
         small_number_suppression=True,
     ),     
 
@@ -297,7 +297,7 @@ measures = [
         id="hosp_rate_binary",
         numerator="admitted_binary",
         denominator="population",
-        group_by=['sgss_gp_cov_admitted_binary'],
+        # group_by=['sgss_gp_cov_admitted_binary'],
         small_number_suppression=True,
     ),
 
@@ -305,7 +305,7 @@ measures = [
         id="hosp_rate_sex_binary",
         numerator="admitted_binary",
         denominator="population",
-        group_by=["sex", 'sgss_gp_cov_admitted_binary'],
+        group_by=["sex"]#, 'sgss_gp_cov_admitted_binary'],
         small_number_suppression=True,
     ),
 
@@ -313,7 +313,7 @@ measures = [
         id="hosp_rate_age_cat_binary",
         numerator="admitted_binary",
         denominator="population",
-        group_by=['age_cat', 'sgss_gp_cov_admitted_binary'],
+        group_by=['age_cat']#, 'sgss_gp_cov_admitted_binary'],
         small_number_suppression=True,
     ),    
     
