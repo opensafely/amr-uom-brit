973460fc1c36b7d2fe1c1096df85c723ca058c09
BillyZhongUOM <61462861+BillyZhongUOM@users.noreply.github.com>
2022-06-01 12:41:50 +0100

    update

--- analysis/description/figure_2.R
+++ analysis/description/figure_2.R
@@ -1,4 +1,4 @@
-### This script is for preparing the variables for broad spectrum antibiotics model ###
+### This script is for preparing the variables for repeat antibiotics model ###
 library("dplyr")
 library("tidyverse")
 library("lubridate")
@@ -38,11 +38,8 @@ df <- df %>% mutate(age_group = case_when(age>3 & age<=15 ~ "<16",
 
 
 ### prepare var ### without Amoxicillin ###
-broadtype <- c("Ampicillin","Co-amoxiclav","Moxifloxacin","Cefaclor","Cefadroxil",
-               "Cefuroxime", "Cefalexin","Cefazolin","Cefixime","Cefotaxime","Cefoxitin","Cefradine",
-               "Cefpirome","Ceftazidime","Ceftriaxone", "Cefprozil","Ciprofloxacin","Co-fluampicil",
-               "Doripenem","Ertapenem", "Cilastatin","Cefamandole","Levofloxacin" , 
-               "Meropenem" ,"Nalidixic acid","Norfloxacin", "Ofloxacin","Cefpodoxime","Cefepime")
+
+df <- df %>% filter (incidental == 1)
 
 start_covid = as.Date("2020-04-01")
 covid_adjustment_period_from = as.Date("2020-03-01")
@@ -54,17 +51,17 @@ df$cal_mon <- month(df$date)
 df$time <- as.numeric(df$cal_mon+(df$cal_year-2019)*12)
 DF <- df
 
-### Broad spectrum by age
+### repeat by age
 
-df.broad <- df %>% filter(type %in% broadtype ) 
-df.broad_total <- df.broad %>% group_by(time,age_group) %>% summarise(
+df.repeat <- df %>% filter(repeat_ab == 1) 
+df.repeat_total <- df.repeat %>% group_by(time,age_group) %>% summarise(
   numOutcome = n(),
 )
 
 df.all <-  df %>% group_by(time,age_group) %>% summarise(
   numEligible = n(),
 )
-df.model <- merge(df.broad_total,df.all,by=c("time","age_group"))
+df.model <- merge(df.repeat_total,df.all,by=c("time","age_group"))
 
 df.model <- df.model %>% mutate(mon = case_when(time>=1 & time<=12 ~ time,
                                                 time>=13 & time<=24 ~ time-12,
@@ -86,7 +83,7 @@ figure_age_strata <- ggplot(df.model, aes(x = as.Date("2019-01-01"), y = value, 
   scale_x_date(date_labels = "%Y", breaks = "1 year") +
   geom_vline(xintercept = c(start_covid, 
                             covid_adjustment_period_from), col = 1, lwd = 1)+
-  labs(x = "Date", y = "% of broad-sepctrum prescription", title = "", colour = "Age", fill = "Age") +
+  labs(x = "Date", y = "% of repeat prescription", title = "", colour = "Age", fill = "Age") +
   theme_classic()  +
   theme(axis.title = element_text(size = 18),
         axis.text = element_text(size = 12),
@@ -101,21 +98,22 @@ figure_age_strata
 
 ggsave(
   plot= figure_age_strata,
-  filename="figure_age_strata.jpeg", path=here::here("output"),
+  filename="figure_2_age_strata.jpeg", path=here::here("output"),
 )  
-write_csv(df.model, here::here("output", "figure_age_strata_table.csv"))
+write_csv(df.model, here::here("output", "figure_2_age_strata_table.csv"))
 rm(df.broad_total,df.all,df.model)
 
-### Broad spectrum by sex
+### repeat by sex
 
-df.broad_total <- df.broad %>% group_by(time,sex) %>% summarise(
+df.repeat <- df %>% filter(repeat_ab == 1) 
+df.repeat_total <- df.repeat %>% group_by(time,sex) %>% summarise(
   numOutcome = n(),
 )
 
 df.all <-  df %>% group_by(time,sex) %>% summarise(
   numEligible = n(),
 )
-df.model <- merge(df.broad_total,df.all,by=c("time","sex"))
+df.model <- merge(df.repeat_total,df.all,by=c("time","sex"))
 
 df.model <- df.model %>% mutate(mon = case_when(time>=1 & time<=12 ~ time,
                                                 time>=13 & time<=24 ~ time-12,
@@ -137,7 +135,7 @@ figure_sex_strata <- ggplot(df.model, aes(x = as.Date("2019-01-01"), y = value, 
   scale_x_date(date_labels = "%Y", breaks = "1 year") +
   geom_vline(xintercept = c(start_covid, 
                             covid_adjustment_period_from), col = 1, lwd = 1)+
-  labs(x = "Date", y = "% of broad-sepctrum prescription", title = "", colour = "Gender", fill = "Gender") +
+  labs(x = "Date", y = "% of repeat prescription", title = "", colour = "Gender", fill = "Gender") +
   theme_classic()  +
   theme(axis.title = element_text(size = 18),
         axis.text = element_text(size = 12),
@@ -152,20 +150,21 @@ figure_sex_strata
 
 ggsave(
   plot= figure_sex_strata,
-  filename="figure_sex_strata.jpeg", path=here::here("output"),
+  filename="figure_2_sex_strata.jpeg", path=here::here("output"),
 )  
-write_csv(df.model, here::here("output", "figure_sex_strata_table.csv"))
+write_csv(df.model, here::here("output", "figure_2_sex_strata_table.csv"))
 rm(df.broad_total,df.all,df.model)
-### Broad spectrum by region
+### Repeat by region
 
-df.broad_total <- df.broad %>% group_by(time,region) %>% summarise(
+df.repeat <- df %>% filter(repeat_ab == 1) 
+df.repeat_total <- df.repeat %>% group_by(time,region) %>% summarise(
   numOutcome = n(),
 )
 
 df.all <-  df %>% group_by(time,region) %>% summarise(
   numEligible = n(),
 )
-df.model <- merge(df.broad_total,df.all,by=c("time","region"))
+df.model <- merge(df.repeat_total,df.all,by=c("time","region"))
 
 df.model <- df.model %>% mutate(mon = case_when(time>=1 & time<=12 ~ time,
                                                 time>=13 & time<=24 ~ time-12,
@@ -187,7 +186,7 @@ figure_region_strata <- ggplot(df.model, aes(x = as.Date("2019-01-01"), y = valu
   scale_x_date(date_labels = "%Y", breaks = "1 year") +
   geom_vline(xintercept = c(start_covid, 
                             covid_adjustment_period_from), col = 1, lwd = 1)+
-  labs(x = "Date", y = "% of broad-sepctrum prescription", title = "", colour = "Region", fill = "Region") +
+  labs(x = "Date", y = "% of repeat prescription", title = "", colour = "Region", fill = "Region") +
   theme_classic()  +
   theme(axis.title = element_text(size = 18),
         axis.text = element_text(size = 12),
@@ -202,7 +201,7 @@ figure_region_strata
 
 ggsave(
   plot= figure_region_strata,
-  filename="figure_region_strata.jpeg", path=here::here("output"),
+  filename="figure_2_region_strata.jpeg", path=here::here("output"),
 )  
-write_csv(df.model, here::here("output", "figure_region_strata_table.csv"))
+write_csv(df.model, here::here("output", "figure_2_region_strata_table.csv"))
 
