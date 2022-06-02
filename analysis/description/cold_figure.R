2300129bf9a6fd34501b56eae42555f621fdad9d
BillyZhongUOM <61462861+BillyZhongUOM@users.noreply.github.com>
2022-06-02 22:06:11 +0100

    update

--- analysis/description/cold_figure.R
+++ analysis/description/cold_figure.R
@@ -10,32 +10,7 @@ library("finalfit")
 rm(list=ls())
 setwd(here::here("output", "measures"))
 
-DF <- readRDS("cleaned_indication_ab.rds")
-
-# recode
-DF$date <- as.Date(DF$date)
-DF <- DF %>% filter(date <= as.Date("2021-12-31"))
-DF <- DF %>% filter(age > 3)
-setwd(here::here("output"))
-df2.1 <- read_csv("prepared_var_2019.csv") %>% select(-c(age,sex))
-df2.2 <- read_csv("prepared_var_2020.csv") %>% select(-c(age,sex))
-df2.3 <- read_csv("prepared_var_2021.csv") %>% select(-c(age,sex))
-df2 <- rbind(df2.1,df2.2,df2.3)
-df <- merge(DF,df2,by=c("patient_id"))
-rm(DF,df2)
-
-df$ethnicity_6 <- as.factor(df$ethnicity_6)
-df$imd <- as.factor(df$imd)
-df$region <- as.factor(df$region)
-df$charlsonGrp <- as.factor(df$charlsonGrp)
-df$ab12b4 <- as.factor(df$ab12b4)
-df <- df %>% dplyr::select(patient_id,incidental,infection,type,repeat_ab,age,sex,ethnicity_6,region,charlsonGrp,imd,ab12b4,date)
-df <- df %>% filter (df$sex=="M"|df$sex=="F")
-df <- df %>% mutate(age_group = case_when(age>3 & age<=15 ~ "<16",
-                                          age>=16 & age<=44 ~ "16-44",
-                                          age>=45 & age<=64 ~ "45-64",
-                                          age>=65 ~ "65+"))
-
+df <- readRDS("cohort_1.rds")
 
 ### cold : repeat/percentage
 
