76fc09cc48e44a9c16f696ede10b72a17eeefa9e
BillyZhongUOM <61462861+BillyZhongUOM@users.noreply.github.com>
2022-06-22 10:54:17 +0100

    Update broad_percentage_op.R

--- analysis/plot/broad_percentage_op.R
+++ analysis/plot/broad_percentage_op.R
@@ -7,8 +7,8 @@ df1 <- read_csv(
   col_types = cols_only(
     practice = col_integer(),
     # Outcomes
-    broad_spectrum_op  = col_double(),
-    antibacterial_brit = col_double(),
+    broad_spect_op  = col_integer(),
+    antibacterial_brit = col_integer(),
     value = col_double(),
     
     # Date
@@ -18,15 +18,11 @@ df1 <- read_csv(
   na = character()
 )
 
-# remove last month data
-last.date=max(df1$date)
-df1=df1%>% filter(date != last.date)
-first_mon=format(min(df1$date),"%m-%Y")
-last_mon= format(max(df1$date),"%m-%Y")
+df1 <- df1 %>% filter (date <= as.Date("2021-12-31"))
 
 
 plot1 <- df1 %>% group_by(date) %>% summarise(
-  b_count = sum(broad_spectrum_op, na.rm = TRUE),
+  b_count = sum(broad_spect_op, na.rm = TRUE),
   ab_count = sum(antibacterial_brit, na.rm = TRUE)
 )
 
@@ -36,21 +32,27 @@ plot1$cal_year <- year(plot1$date)
 plot1$year <- as.factor(plot1$cal_year)
 plot1$mon <- as.factor(plot1$cal_mon)
 
-p <- ggplot(plot1, aes(x=mon, y=prop, group=year)) +
-  geom_line(aes(color=year))+
-  geom_point(aes(color=year))+
-  scale_color_brewer(palette="Paired")+
-  theme_minimal()+
-  scale_y_continuous(labels = scales::percent,breaks=seq(0, 1, by = 0.001))+
+
+p_1 <- ggplot(plot1, aes(y=prop, x=date)) + 
+  geom_line(color = "coral2", size = 0.5)+ 
+  scale_y_continuous(labels = scales::percent,breaks=seq(0, 0.15, by = 0.0025))+
+  annotate(geom = "rect", xmin = as.Date("2021-01-01"),xmax = as.Date("2021-04-01"),ymin = -Inf, ymax = Inf,fill="grey80", alpha=0.5)+
+  annotate(geom = "rect", xmin = as.Date("2020-11-01"),xmax = as.Date("2020-12-01"),ymin = -Inf, ymax = Inf,fill="grey80", alpha=0.5)+
+  annotate(geom = "rect", xmin = as.Date("2020-03-01"),xmax = as.Date("2020-06-01"),ymin = -Inf, ymax = Inf,fill="grey80", alpha=0.5)+
   labs(
-    title = "Proportion of broad-spectrum antibiotics prescribed",
-    subtitle = paste(first_mon,"-",last_mon),
-    x = "Month",
-    y = "broad-spectrum antibiotics prescribing %")
-p
+    title = "Broad-spectrum",
+    caption = "National lockdown time in grey background. ",
+    y = "Percentage",
+    x=""
+  )+
+  theme(axis.text.x=element_text(angle=60,hjust=1))+
+  scale_x_date(date_labels = "%m-%Y", date_breaks = "1 month")
+
+p_1
+
 
 ggsave(
-  plot= p,
+  plot= p_1,
   filename="broad_percentage_op.jpeg", path=here::here("output"),
 )
 
