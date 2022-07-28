3196bb5ba46c5549994e03da7bf08fac9bed1df5
BillyZhongUOM <61462861+BillyZhongUOM@users.noreply.github.com>
2022-07-28 10:37:21 +0100

    update

---
+++ analysis/xiaomin_ab/TPP_ab_frequency.R
@@ -0,0 +1,28 @@
+library("tidyverse") 
+library('dplyr')
+library('lubridate')
+
+
+df <- read_csv(
+  here::here("output", "input_ab_extraction.csv.gz"),  
+  col_types = cols_only(
+    antibacterial_brit = col_double(),
+    antibacterial_brit_abtype = col_character(),
+    patient_id = col_double()
+  ),
+  na = character()
+)
+
+df.all <- df %>% summarise(
+    ab_count = sum(antibacterial_brit)
+)
+
+df.abtype <- df %>% group_by(antibacterial_brit_abtype) %>% summarise(
+    type_count = sum(antibacterial_brit)
+)
+
+df.abtype $prop <- df.abtype$type_count*100/df.all$ab_count
+
+write_csv(df.abtype , here::here("output", "TPP_ab_frequency.csv"))
+
+
