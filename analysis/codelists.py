99e324ed1a3d335aed6b29892f7df901abd8ea0c
ali-fahmi <alifahmi1990@gmail.com>
2022-08-23 16:46:29 +0100

    update abs codelist; remove data of 2 months before

--- analysis/codelists.py
+++ analysis/codelists.py
@@ -21,10 +21,12 @@ from cohortextractor import codelist, codelist_from_csv, combine_codelists
 antibacterials_codes = codelist_from_csv(
     "codelists/opensafely-antibacterials.csv", system="snomed", column="dmd_id"
 )
+# antibacterials_codes_brit = codelist_from_csv(
+#     "codelists/user-rriefu-antibiotics_dmd.csv", system="snomed", column="dmd_id"
+# )
 antibacterials_codes_brit = codelist_from_csv(
-    "codelists/user-rriefu-antibiotics_dmd.csv", system="snomed", column="dmd_id"
+    "codelists/user-BillyZhongUOM-brit_new_dmd.csv", system="snomed", column="dmd_id", category_column="type",
 )
-
 
 ### broad antibacterials
 broad_spectrum_antibiotics_codes = codelist_from_csv(
