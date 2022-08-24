a5424930303829858182eb3b059b5fc3eee7149d
rriefu <rriefu@gmail.com>
2022-08-24 14:40:25 +0100

    update ethnicity

--- analysis/study_definition_outcome.py
+++ analysis/study_definition_outcome.py
@@ -142,15 +142,6 @@ study = StudyDefinition(
             },
     ),
 
-## re-extract ethnicity
-    ethnicity=patients.with_ethnicity_from_sus(
-        returning="group_6",
-        use_most_frequent_code=True,
-        return_expectations={
-            "category": {"ratios": {"1": 0.8, "5": 0.1, "3": 0.1}},
-            "incidence": 0.75,
-        },
-    ),
     
 # data check	
     ## de-register after start date	
