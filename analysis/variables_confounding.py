4baa0ab4f3850818f1d395dec383b1b0f9f9120d
rriefu <rriefu@gmail.com>
2022-08-24 14:52:00 +0100

    update ethnicity

--- analysis/variables_confounding.py
+++ analysis/variables_confounding.py
@@ -55,13 +55,23 @@ def generate_confounding_variables(index_date_variable):
     #     }
     # ),
 
+    # # self-reported ethnicity 
+    # ethnicity=patients.with_these_clinical_events(
+    #     ethnicity_codes,
+    #     returning="category",
+    #     find_last_match_in_period=True,
+    #     include_date_of_match=False,
+    #     return_expectations={
+    #         "category": {"ratios": {"1": 0.8, "5": 0.1, "3": 0.1}},
+    #         "incidence": 0.75,
+    #     },
+    # ),
 
-    # self-reported ethnicity 
-    ethnicity=patients.with_these_clinical_events(
-        ethnicity_codes,
-        returning="category",
-        find_last_match_in_period=True,
-        include_date_of_match=False,
+
+## re-extract ethnicity
+    ethnicity=patients.with_ethnicity_from_sus(
+        returning="group_6",
+        use_most_frequent_code=True,
         return_expectations={
             "category": {"ratios": {"1": 0.8, "5": 0.1, "3": 0.1}},
             "incidence": 0.75,
