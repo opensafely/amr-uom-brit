2bada00a5349b5d434eb1e2b2227c2d6cbef3568
BillyZhongUOM <61462861+BillyZhongUOM@users.noreply.github.com>
2022-09-01 21:51:36 +0100

    update

--- analysis/conversion/cohort_1_id_extraction.R
+++ analysis/conversion/cohort_1_id_extraction.R
@@ -24,33 +24,33 @@ write_csv(overall_counts, here::here("output", "cohort_1_pat_num_2019.csv"))
 
 rm(DF,overall_counts)
 
-# ### id extraction 2020 ###
+### id extraction 2020 ###
 
-# DF <- readRDS("process_1_2020.rds")
+DF <- readRDS("process_1_2020.rds")
 
-# # recode
-# DF$date <- as.Date(DF$date)
-# DF <- DF %>% select(patient_id, "patient_index_date" = date)
-# num_records <- length(DF$patient_id)
-# num_pats <- length(unique(DF$patient_id))
-# DF <- distinct(DF, patient_id, .keep_all = TRUE)
-# overall_counts <- as.data.frame(cbind(num_records,num_pats))
-# write_csv(DF, here::here("output", "cohort_1_id_2020.csv"))
-# write_csv(overall_counts, here::here("output", "cohort_1_pat_num_2020.csv"))
+# recode
+DF$date <- as.Date(DF$date)
+DF <- DF %>% select(patient_id, "patient_index_date" = date)
+num_records <- length(DF$patient_id)
+num_pats <- length(unique(DF$patient_id))
+DF <- distinct(DF, patient_id, .keep_all = TRUE)
+overall_counts <- as.data.frame(cbind(num_records,num_pats))
+write_csv(DF, here::here("output", "cohort_1_id_2020.csv"))
+write_csv(overall_counts, here::here("output", "cohort_1_pat_num_2020.csv"))
 
-# rm(DF,overall_counts)
+rm(DF,overall_counts)
 
-# ### id extraction 2021 ###
+### id extraction 2021 ###
 
-# DF <- readRDS("process_1_2021.rds")
+DF <- readRDS("process_1_2021.rds")
 
-# # recode
-# DF$date <- as.Date(DF$date)
-# DF <- DF %>% select(patient_id, "patient_index_date" = date)
-# num_records <- length(DF$patient_id)
-# num_pats <- length(unique(DF$patient_id))
-# DF <- distinct(DF, patient_id, .keep_all = TRUE)
-# overall_counts <- as.data.frame(cbind(num_records,num_pats))
-# write_csv(DF, here::here("output", "cohort_1_id_2021.csv"))
-# write_csv(overall_counts, here::here("output", "cohort_1_pat_num_2021.csv"))
+# recode
+DF$date <- as.Date(DF$date)
+DF <- DF %>% select(patient_id, "patient_index_date" = date)
+num_records <- length(DF$patient_id)
+num_pats <- length(unique(DF$patient_id))
+DF <- distinct(DF, patient_id, .keep_all = TRUE)
+overall_counts <- as.data.frame(cbind(num_records,num_pats))
+write_csv(DF, here::here("output", "cohort_1_id_2021.csv"))
+write_csv(overall_counts, here::here("output", "cohort_1_pat_num_2021.csv"))
 
