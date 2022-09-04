c0a29591ee908a35413bd416a0d3756c30a2e4bf
BillyZhongUOM <61462861+BillyZhongUOM@users.noreply.github.com>
2022-09-04 11:50:41 +0100

    update

--- analysis/descriptive/cohort_1_baseline_table_pat_one.R
+++ analysis/descriptive/cohort_1_baseline_table_pat_one.R
@@ -79,3 +79,21 @@ colsfortab <- colnames(dttable)
 dttable %>% summary_factorlist(explanatory = colsfortab) -> t
 
 write_csv(t, here::here("output", "cohort_1_baseline_all.csv"))
+
+rm(dttable)
+
+
+set.seed(777)
+
+df0_1pat.all <- DF %>% dplyr::group_by(patient_id) %>%
+  dplyr::arrange(date, .group_by=TRUE) %>%
+  sample_n(1)
+
+# generate data table 
+dttable <- select(df0_1pat.21,age_group,sex,imd,region,charlsonGrp,ethnicity_6)
+
+# columns for baseline table
+colsfortab <- colnames(dttable)
+dttable %>% summary_factorlist(explanatory = colsfortab) -> t
+#str(t)
+write_csv(t, here::here("output", "cohort_1_baseline_all_pat_one.csv"))
