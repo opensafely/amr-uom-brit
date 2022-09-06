5562f4af04a953b673e6bc15707a9c05d050fdce
BillyZhongUOM <61462861+BillyZhongUOM@users.noreply.github.com>
2022-09-06 14:46:27 +0100

    update

---
+++ analysis/study_definition_covid.py
@@ -0,0 +1,96 @@
+
+from cohortextractor import (
+    StudyDefinition,
+    patients,
+    codelist,
+    filter_codes_by_category,
+    Measure
+)
+
+###### Code lists
+from codelists import *
+
+# DEFINE STUDY TIME #
+from datetime import datetime
+start_date = "2019-01-01"
+end_date = "2021-12-31"
+
+## Define study population and variables
+study = StudyDefinition(
+
+    # Configure the expectations framework
+    default_expectations={
+        "date": {"earliest": start_date, "latest": end_date},
+        "rate": "uniform",
+        "incidence": 0.1,
+    },
+    # Set index date to start date
+    index_date=start_date,
+    # Define the study population
+    population=patients.satisfying(
+        """
+        NOT has_died
+        AND
+        registered
+        AND
+        age
+        AND
+        has_follow_up_previous_year
+        AND
+        (sex = "M" OR sex = "F")
+        """,
+
+        has_died=patients.died_from_any_cause(
+            on_or_before="index_date",
+            returning="binary_flag",
+        ),
+
+        registered=patients.satisfying(
+            "registered_at_start",
+            registered_at_start=patients.registered_as_of("index_date"),
+        ),
+
+        has_follow_up_previous_year=patients.registered_with_one_practice_between(
+            start_date="index_date - 1 year",
+            end_date="index_date",
+            return_expectations={"incidence": 0.95},
+        ),
+    ),
+
+    ########## patient demographics to group_by for measures:
+    ### Age
+    age=patients.age_as_of(
+        "index_date",
+        return_expectations={
+            "rate": "universal",
+            "int": {"distribution": "population_ages"},
+            "incidence": 0.001
+        },
+    ),
+    
+    ### Sex
+    sex=patients.sex(
+        return_expectations={
+            "rate": "universal",
+            "category": {"ratios": {"M": 0.49, "F": 0.51}},
+        }
+    ),
+
+
+    ### Practice
+    practice=patients.registered_practice_as_of(
+        "index_date",
+        returning="pseudo_id",
+        return_expectations={"int": {"distribution": "normal",
+                                     "mean": 25, "stddev": 5}, "incidence": 1}
+    ),
+
+    ## Covid positive test result-SGSS
+    Positive_test_event=patients.with_test_result_in_sgss(
+        pathogen="SARS-CoV-2",
+        test_result="positive",
+        between=["index_date", "last_day_of_month(index_date)"],
+        returning = "binary_flag",
+        return_expectations={"incidence": 0.5},
+    ),
+)
