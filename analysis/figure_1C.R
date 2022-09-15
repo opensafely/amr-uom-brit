76b5a47bd0d94c19f79475ac89498490948f89bc
BillyZhongUOM <61462861+BillyZhongUOM@users.noreply.github.com>
2022-09-15 10:28:30 +0100

    Update figure_1C.R

--- analysis/figure_1C.R
+++ analysis/figure_1C.R
@@ -8,12 +8,16 @@ library("ggsci")
 rm(list=ls())
 setwd(here::here("output", "measures"))
 
-col_spec <-cols_only(  broad_ab_binary = col_number(),
-                       AB_given_14D_window = col_number(),
+col_spec1 <-cols_only(AB_given_14D_window = col_number(),
                        Positive_test_event = col_number(),
                        value = col_number(),
                        date = col_date(format = "")
 )
+
+col_spec2 <-cols_only(Broad_given_14D_window = col_number(),
+                       date = col_date(format = "")
+)
+
 
 
 lockdown_1_start = as.Date("2020-03-26")
@@ -26,40 +30,50 @@ lockdown_3_end = as.Date("2021-03-08")
 
 
 
-df <- read_csv("measure_14D_window_ab.csv",
-               col_types = col_spec)
+df1 <- read_csv("measure_14D_window_ab.csv",
+               col_types = col_spec1) %>% filter(date >=as.Date("2020-03-01"))
 
-df <- df %>% filter(date >=as.Date("2020-03-01"))
+df2 <- read_csv("measure_14D_window_broad.csv",
+               col_types = col_spec2) %>% filter(date >=as.Date("2020-03-01"))
 
-df.allab <- df %>%
-  group_by(date)%>%
-  summarise(AB_given_14D_window=sum(AB_given_14D_window,na.rm=TRUE),
-            Positive_test_event=sum(Positive_test_event,na.rm=TRUE)) %>%
-  mutate(value = round(AB_given_14D_window/Positive_test_event,digits = 3))
+df <- left_join(df1,df2,by = "date")
+df$ab_rate <- round(df$value,digits = 3)
+df$broad_rate <- round(df$Broad_given_14D_window/df$AB_given_14D_window,digits = 3)
 
-df.broad <- df %>% group_by(date) %>% 
-  mutate(Positive_test_event=sum(Positive_test_event,na.rm=TRUE))
+train_sec <- function(primary, secondary, na.rm = TRUE) {
+  # Thanks Henry Holm for including the na.rm argument!
+  from <- range(secondary, na.rm = na.rm)
+  to   <- range(primary, na.rm = na.rm)
+  # Forward transform for the data
+  forward <- function(x) {
+    rescale(x, from = from, to = to)
+  }
+  # Reverse transform for the secondary axis
+  reverse <- function(x) {
+    rescale(x, from = to, to = from)
+  }
+  list(fwd = forward, rev = reverse)
+}
 
-df.broad <- df.broad %>% filter(broad_ab_binary == 1)
-df.broad$value <-  round(df.broad$AB_given_14D_window/df.broad$Positive_test_event,digits = 3)
-df.broad <- df.broad[,-1]
-df.allab $ type <- "all antibiotics"
-df.broad $ type <- "broad-spectrum"
+sec <- with(df, train_sec(c(0, max(ab_rate)),
+                          c(0, max(broad_rate))))
 
-df <- bind_rows(df.allab,df.broad)
 
-p <- ggplot(df, aes(x=date, y=value, group = type, color = type))+
-  annotate(geom = "rect", xmin=lockdown_1_start, xmax=lockdown_1_end,ymin = -Inf, ymax = Inf,fill="#DEC0A0", alpha=0.5)+
-  annotate(geom = "rect", xmin=lockdown_2_start, xmax=lockdown_2_end,ymin = -Inf, ymax = Inf,fill="#DEC0A0", alpha=0.5)+
-  annotate(geom = "rect", xmin=lockdown_3_start, xmax=lockdown_3_end,ymin = -Inf, ymax = Inf,fill="#DEC0A0", alpha=0.5)+
-  geom_line(aes(y = value), lwd = 0.8)+ scale_color_manual(values=c("#0F5DC9", "#BA6A16"))+
+
+p <- ggplot(df, aes(x=date, y=sec$fwd(broad_rate))) +
+  geom_rect(aes(xmin=lockdown_1_start, xmax=lockdown_1_end, ymin=-Inf, ymax=Inf),fill = "#DEC0A0")+
+  geom_rect(aes(xmin=lockdown_2_start, xmax=lockdown_2_end, ymin=-Inf, ymax=Inf),fill = "#DEC0A0")+
+  geom_rect(aes(xmin=lockdown_3_start, xmax=lockdown_3_end, ymin=-Inf, ymax=Inf),fill = "#DEC0A0")+
+  geom_line(aes(y = ab_rate), colour = "#0F5DC9",size = 0.8) +
+  geom_line(aes(y = sec$fwd(broad_rate)), colour = "#BA6A16") +
   scale_x_date(date_labels = "%Y %b", breaks = "3 months") +
+  scale_y_continuous(sec.axis = sec_axis(~sec$rev(.), name = "broad-spectrum rate"))+
   labs(x = "", y = "antibiotic prescribed in 14-day window tested positive for SARS-Cov-2") +
   theme_bw() +
   theme(axis.text.x=element_text(angle=60,hjust=1))
-            
 p
-            
+
+
 ggsave(p, width = 12, height = 6, dpi = 640,
                    filename="figure_1C.jpeg", path=here::here("output"),
             )  
