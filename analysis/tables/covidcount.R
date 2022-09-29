3fb1fd6af08ff4554fb0ac3f07d149291d675fca
BillyZhongUOM <61462861+BillyZhongUOM@users.noreply.github.com>
2022-09-29 11:09:03 +0100

    update

---
+++ analysis/tables/covidcount.R
@@ -0,0 +1,32 @@
+
+
+## install package
+#install.packages("tableone")
+
+## Import libraries---
+library("tidyverse") 
+library('plyr')
+library('dplyr')
+library('lubridate')
+library('stringr')
+library("data.table")
+library("ggpubr")
+library("finalfit")
+
+setwd(here::here("output"))
+
+### read data  ###
+### use new synthesised .rds files for faster loading
+
+df.20 <- read.csv('input_covidnum_2020.csv.gz')
+df.21 <- read.csv('input_covidnum_2021.csv.gz')
+df.22 <- read.csv('input_covidnum_2022.csv.gz')
+
+df.20 <- df.20%>%filter(Positive_test_event==1)%>%summarise(count20=length(patient_id))
+df.21 <- df.21%>%filter(Positive_test_event==1)%>%summarise(count21=length(patient_id))
+df.22 <- df.22%>%filter(Positive_test_event==1)%>%summarise(count22=length(patient_id))
+
+df <-cbind(df.20,df.21,df.22)
+
+write_csv(df, here::here("output", "covid_count.csv"))
+
