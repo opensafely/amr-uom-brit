19b72fd9ed13895c97cc3679c8fc0d8d46a38254
BillyZhongUOM <61462861+BillyZhongUOM@users.noreply.github.com>
2022-11-10 14:30:51 +0000

    update

--- analysis/figure_3_bar.R
+++ analysis/figure_3_bar.R
@@ -50,22 +50,37 @@ df$Broad_given_14D_window <- plyr::round_any(df$Broad_given_14D_window, 5)
 df$AB_given_14D_window <- plyr::round_any(df$AB_given_14D_window, 5)
 df$broad_rate <- round(df$Broad_given_14D_window/df$AB_given_14D_window,digits = 3)
 
+df$narrow_given_14_window <- df$AB_given_14D_window - df$Broad_given_14D_window
+df$no_ab_given_14_window <- df$Positive_test_event - df$AB_given_14D_window
 
-p <- ggplot(df, aes(x=date)) +
+df_broad <- select(df,c("Broad_given_14D_window","date"))
+df_broad$group <- "broad"
+names(df_broad) <- c("count","date","group")
+df_narrow <- select(df,c("narrow_given_14_window","date"))
+df_narrow$group <- "narrow"
+names(df_narrow) <- c("count","date","group")
+df_noab <- select(df,c("no_ab_given_14_window","date"))
+df_noab$group <- "noab"
+names(df_noab) <- c("count","date","group")
+
+df_plot <- bind_rows(df_broad,df_narrow,df_noab)
+
+p <- ggplot(df_plot, aes(x = date, y = count, fill = group)) + 
   geom_rect(aes(xmin=lockdown_1_start, xmax=lockdown_1_end, ymin=-Inf, ymax=Inf),fill = "#DEC0A0")+
   geom_rect(aes(xmin=lockdown_2_start, xmax=lockdown_2_end, ymin=-Inf, ymax=Inf),fill = "#DEC0A0")+
   geom_rect(aes(xmin=lockdown_3_start, xmax=lockdown_3_end, ymin=-Inf, ymax=Inf),fill = "#DEC0A0")+
-  geom_line(aes(y = ab_rate), colour = "#0F5DC9",size = 0.8) +
-  geom_line(aes(y = broad_rate), colour = "#BA6A16") +
-  scale_x_date(date_labels = "%Y %b", breaks = "3 months") +
-  scale_y_continuous(ymin=0, ymax=Inf,labels = scales::percent)+
+  geom_bar(stat = "identity") +
+  scale_x_date(date_labels = "%Y %b", breaks = "1 month") +
   theme_bw() +
-  theme(axis.text.x=element_text(angle=60,hjust=1))
+  theme(axis.text.x=element_text(angle=60,hjust=1)) +
+  scale_fill_manual(values = c("#DADAEB", "#9E9AC8", "#6A51A3"),
+                         name="",
+                         breaks=c("broad", "narrow", "noab"),
+                         labels=c("broad-spectrum", "narrow-spectrum", "no antibiotic"))
 p
 
-
 ggsave(p, width = 12, height = 6, dpi = 640,
-                   filename="figure_3.jpeg", path=here::here("output"),
+                   filename="figure_3_bar.jpeg", path=here::here("output"),
             )  
 
-write_csv(df, here::here("output", "figure_3_table.csv"))
+write_csv(df, here::here("output", "figure_3_bar_table.csv"))
