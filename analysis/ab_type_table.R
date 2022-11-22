bd7318479b7e3cfc20202d2da121103e080a262b
BillyZhongUOM <61462861+BillyZhongUOM@users.noreply.github.com>
2022-11-22 23:56:00 +0000

    update

--- analysis/ab_type_table.R
+++ analysis/ab_type_table.R
@@ -61,3 +61,33 @@ df.frequency2$prop <- df.frequency2$n_count/all_count
 df.frequency2$prop <- round(df.frequency2$prop,digits = 3)
 
 write_csv(df.frequency2, here::here("output", "ab_type_table_2D.csv"))
+
+#### 7 Day ####
+setwd(here::here("output", "measures"))
+
+col_spec2 <-cols_only(antibiotic_type = col_character(),
+                     AB_given_7D_window = col_number(),
+                     Positive_test_event = col_number(),
+                     date = col_date(format = "")
+)
+
+df20.2 <- read_csv("measure_covid_window_7D_top5_ab_by_type.csv",
+                 col_types = col_spec2) %>% filter(date >=as.Date("2020-03-01"))
+df21.2 <- read_csv("measure_21_covid_window_7D_top5_ab_by_type.csv",
+                 col_types = col_spec2)
+
+df2 <- bind_rows(df20.2,df21.2)
+
+df.frequency2 <- df2 %>% group_by(antibiotic_type) %>% summarise(
+  n_count = sum(AB_given_7D_window)
+)
+
+all_count <- sum(df2$AB_given_7D_window)
+
+
+df.frequency2$n_count <- plyr::round_any(df.frequency2$n_count, 5)
+df.frequency2$prop <- df.frequency2$n_count/all_count
+df.frequency2$prop <- round(df.frequency2$prop,digits = 3)
+
+write_csv(df.frequency2, here::here("output", "ab_type_table_7D.csv"))
+
