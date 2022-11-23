11f932e5417b6382d8a879b0634c319a97f7b468
ali-fahmi <alifahmi1990@gmail.com>
2022-11-23 22:07:32 +0000

    end dates of study defs set to 2020-01-01

--- analysis/study_definition_hospitalisation_throat.py
+++ analysis/study_definition_hospitalisation_throat.py
@@ -32,7 +32,7 @@ from codelists import *
 from datetime import datetime
 
 start_date = "2019-01-01"
-end_date = datetime.today().strftime('%Y-%m-%d')
+end_date = "2020-01-01" #datetime.today().strftime('%Y-%m-%d')
 
 ## Define study population and variables
 study = StudyDefinition(
