7faad7c1667175f770e90e91e9b6ea777fc70dfe
rriefu <rriefu@gmail.com>
2022-11-27 15:58:38 +0000

    add note

--- analysis/process_1.R
+++ analysis/process_1.R
@@ -2,7 +2,7 @@
 # # # # # # # # # # # # # # # # # # # # #
 # This script:
 # 1. define covid admission cohort 
-# 2. define case (icu or death) and control (without any severe outcome)
+# 2. define case and control 
 # # # # # # # # # # # # # # # # # # # # #
 
 ## Import libraries---
@@ -23,27 +23,18 @@ df=df%>%filter(patient_index_date <= as.Date("2021-12-31"))
 # filter cohort
 df=df%>%filter( !is.na(patient_index_date)) # covid hospital case
 
-# df=df%>%
-#   filter(
-#          is.na(died_date_cpns), 
-#          is.na(died_date_ons_covid),
-#          is.na(ons_died_date_before)) 
 
-
-
-## CASE - covid hospital admission (incident covid infeciton, so exclude recors outside 1 month)
+## cohort for covid hospital admission (incident covid infeciton, so exclude recors outside 1 month)
 df=df%>%filter(
          is.na(SGSS_positive_test_date_before),
          is.na(primary_care_covid_date_before), 
          is.na(died_date_cpns_before),
          is.na(died_date_ons_covid_before))
 
-
-
 # calendar month for matching
 df$cal_YM=format(df$patient_index_date,"%Y-%m")
 
-## CASE - covid icu or covid death within 1 month
+## CASE - covid death within 1 month
 df1=df%>%
   filter(icu_days>0|
            !is.na(died_date_ons_covid_after)|
