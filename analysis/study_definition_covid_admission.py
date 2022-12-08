039e65ff82518fc2c9eca6c750d58b1fd7e3df5c
rriefu <rriefu@gmail.com>
2022-12-08 15:18:08 +0000

    Update study_definition_covid_admission.py

--- analysis/study_definition_covid_admission.py
+++ analysis/study_definition_covid_admission.py
@@ -76,7 +76,7 @@ study = StudyDefinition(
 
         has_outcome=patients.admitted_to_hospital(
          #   with_these_primary_diagnoses=covid_codelist,  # only include primary_diagnoses as covid
-            with_these_diagnoses=covid_codelist,  # only include primary_diagnoses as covid
+            with_these_diagnoses=covid_codelist,  
             on_or_after="index_date",
         ),
 
