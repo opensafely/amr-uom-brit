4a0206b7539f15d6659cc2fdc7bc603d4d858d4c
rriefu <rriefu@gmail.com>
2023-02-07 15:31:54 +0000

    add script

--- analysis/process_1.R
+++ analysis/process_1.R
@@ -1,8 +1,8 @@
 
 # # # # # # # # # # # # # # # # # # # # #
 # This script:
-# 1. define control group (SGSS+primary care) 
-# 2. define case group (admittied to hospital) 
+# 1. define covid admission cohort 
+# 2. define case and control 
 # # # # # # # # # # # # # # # # # # # # #
 
 ## Import libraries---
@@ -12,88 +12,63 @@ library('dplyr')
 library('lubridate')
 
 
-#### COVID INFECTION
+#### COVID hospital admission
 
-# impoprt data
-df1 <- read_csv(here::here("output", "input_covid_SGSS.csv"))
-df1=df1%>%filter(patient_index_date <= as.Date("2021-12-31"))
-
-# filter SGSS (exclude SGSS+admission, SGSS+death)
-df1 =df1%>%filter( !is.na(patient_index_date)) # SGSS case
-
-df1=df1%>%
-  filter(is.na(primary_care_covid_date),
-         is.na(covid_admission_date),
-         is.na(died_date_cpns), 
-         is.na(died_date_ons_covid))
+# import data
+df <- read_csv(here::here("output", "input_covid_admission.csv"))
+#df=read_csv("input_covid_admission.csv")
+df=df%>%filter(patient_index_date <= as.Date("2021-12-31"))
 
 
-
-df2<- read_csv(here::here("output", "input_covid_primarycare.csv"))
-df2=df2%>%filter(patient_index_date <= as.Date("2021-12-31"))
-
-df2 =df2%>%filter( !is.na(patient_index_date)) # primary care case
-df2=df2%>%
-  filter(is.na(SGSS_positive_test_date),
-         is.na(covid_admission_date),
-         is.na(died_date_cpns), 
-         is.na(died_date_ons_covid))
+# filter cohort
+df=df%>%filter( !is.na(patient_index_date)) # covid hospital case
 
 
-df=rbind(df1,df2)
-
-# keep earlist covid infection date
-df=df%>%
-  group_by(patient_id)%>%
-  arrange(patient_id,patient_index_date)%>%
-  distinct(patient_id, .keep_all = TRUE)
-
-# calendar month for matching
-df$cal_YM=format(df$patient_index_date,"%Y-%m")
-
-## Control - covid infection without any covid severe outcome within 1 month
-df.0=df%>%
-  filter(
-          is.na(covid_admission_date_after),
-          is.na(died_date_cpns_after),
-          is.na(died_date_ons_covid_after))
-
-
-## definition _1
-# # # CASE - covid infection with hospital admission
-# #  df.1=df%>%
-# #    filter(! is.na(covid_admission_date_after))
-
-## definition _2
-## CASE - covid hospital admission (incident covid infeciton, so exclude recors outside 1 month)
-df.1 <- read_csv(here::here("output", "input_covid_admission.csv"))
-df.1=df.1%>%filter(patient_index_date <= as.Date("2021-12-31"))
-
-df.1 =df.1%>%filter( !is.na(patient_index_date)) # hospital patient
-
-
-df.1=df.1%>%filter(
+## cohort for covid hospital admission (incident covid infeciton, so exclude recors outside 1 month)
+df=df%>%filter(
          is.na(SGSS_positive_test_date_before),
          is.na(primary_care_covid_date_before), 
          is.na(died_date_cpns_before),
          is.na(died_date_ons_covid_before))
 
 # calendar month for matching
-df.1$cal_YM=format(df.1$patient_index_date,"%Y-%m")
+df$cal_YM=format(df$patient_index_date,"%Y-%m")
 
-df.0$case=0
-df.1$case=1
-df=rbind(df.1,df.0)
+## CASE - covid death within 1 month
+df1=df%>%
+  filter(icu_days>0|
+           !is.na(died_date_ons_covid_after)|
+           !is.na(died_date_cpns_after))
 
-# keep earlist covid positive date
-df=df%>%
-  group_by(patient_id)%>%
-  arrange(patient_id,patient_index_date)%>%
-  distinct(patient_id, .keep_all = TRUE)
+# # CASE - covid icu or any death death within 1 month
+# df2=df%>%
+#   filter(icu_days>0|
+#            !is.na(ons_died_date_after)|
+#            !is.na(died_date_cpns_after)|
+#            !is.na(died_date_ons_covid_after))
 
-df0=df%>%filter(case==0)
-df1=df%>%filter(case==1)
+write_csv(df1, here::here("output", "case_covid_icu_death.csv"))
+#write_csv(df2, here::here("output", "case_covid_icu_death_2.csv"))
 
-write_csv(df0, here::here("output", "control_covid_infection.csv"))
 
-write_csv(df1, here::here("output", "case_covid_hosp.csv"))
+
+## Control - covid hospital without any covid severe outcome within 1 month
+df01=df%>%
+  filter(
+    icu_days== 0 | is.na(icu_days),
+    is.na(died_date_cpns_after),
+    is.na(died_date_ons_covid_after))
+
+write_csv(df01, here::here("output", "control_covid_hosp.csv"))
+
+# # Control - covid hospital without any  severe outcome within 1 month
+# df02=df%>%
+#   filter(
+#           icu_days== 0 | is.na(icu_days),
+#           is.na(died_date_cpns_after),
+#           is.na(died_date_ons_covid_after),
+#           is.na(ons_died_date_after))
+
+#write_csv(df02, here::here("output", "control_covid_hosp_2.csv"))
+
+
