f60bc1ffd1877205259a7098948b1b09a72aeea5
BillyZhongUOM <61462861+BillyZhongUOM@users.noreply.github.com>
2023-02-08 12:13:47 +0000

    update

---
+++ analysis/study_definition_control_ab_191.py
@@ -0,0 +1,67 @@
+
+from cohortextractor import (
+    StudyDefinition,
+    patients,
+    #codelist_from_csv,
+    codelist,
+    filter_codes_by_category,
+    #combine_codelists,
+    Measure
+)
+
+###### import matched cohort
+COHORT = "output/control_id_191.csv"
+
+###### Code lists
+from codelists import *
+
+###### Define study time variables
+from datetime import datetime
+start_date = "2019-01-01"
+end_date = "2019-06-30"
+
+####### Import variables
+## Exposure variables: antibiotics 
+from variables_antibiotics_6w import generate_ab_variables_6w
+ab_variables_6w = generate_ab_variables_6w(index_date_variable="patient_index_date")
+
+
+study = StudyDefinition(
+
+    # Configure the expectations framework
+    default_expectations={
+        "date": {"earliest": start_date, "latest": end_date},
+        "rate": "uniform",
+        "incidence": 0.1,
+    },
+
+    # Set index date to start date
+    index_date=start_date,
+   
+    # study population
+    population=patients.which_exist_in_file(COHORT),
+
+    ### patient index date  
+    # case_infection_date
+    patient_index_date=patients.with_value_from_file(
+        COHORT,
+        returning="patient_index_date",
+        returning_type="date",
+    ),
+
+    ## Age
+    age=patients.age_as_of(
+        "patient_index_date",
+        return_expectations={
+            "rate": "universal",
+            "int": {"distribution": "population_ages"},
+            "incidence": 0.001
+        },
+    ),
+
+     **ab_variables_6w,
+    # **confounding_variables,
+    # #**infection_variables,
+    # #**comobidities_variables,
+    # **CCI_variables,
+)
