2084a4a972018b9b0e2a3aa183cd4bf002d27314
BillyZhongUOM <61462861+BillyZhongUOM@users.noreply.github.com>
2023-02-09 14:14:14 +0000

    Update Figure_2A.R

--- analysis/Figure_2A.R
+++ analysis/Figure_2A.R
@@ -79,6 +79,6 @@ p <- forest(dt[,c(1,12:15)],
             theme = tm)
 Figure <- plot(p)
 
-ggsave(Figure, width = 6, height = 10,dpi = 640,
+ggsave(Figure, width = 10, height = 10,dpi = 700,
        filename="Figure_2A.jpeg", path=here::here("output"),
 )  
