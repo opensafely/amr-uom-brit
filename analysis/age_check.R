eec264ba2521712f481959ac3c755ecb788f1bb4
BillyZhongUOM <61462861+BillyZhongUOM@users.noreply.github.com>
2023-03-07 09:29:26 +0000

    Update age_check.R

--- analysis/age_check.R
+++ analysis/age_check.R
@@ -12,8 +12,8 @@ df <- readRDS("output/processed/input_model_c_h.rds")
 
 
 p <- ggplot(df, aes(x=age, color=case)) +
-  geom_histogram(fill="white", position="dodge")+
-  theme(legend.position="top")+scale_color_manual(values=c("#999999", "#E69F00", "#56B4E9"))
+    geom_histogram( color="#e9ecef", alpha=0.6, position = 'identity') +
+    scale_fill_manual(values=c("#69b3a2", "#404080")) 
 
   ggsave(p,dpi = 400,
        filename="age_check.jpeg", path=here::here("output"),
