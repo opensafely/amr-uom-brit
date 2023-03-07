3e5d5fdee87a52b3fbbdeca3f28b85bb1e052efe
BillyZhongUOM <61462861+BillyZhongUOM@users.noreply.github.com>
2023-03-07 09:51:17 +0000

    update

--- analysis/age_check.R
+++ analysis/age_check.R
@@ -11,10 +11,17 @@ library("forestploter")
 df <- readRDS("output/processed/input_model_c_h.rds")
 df$case <- as.factor(df$case)
 
-p <- ggplot(df, aes(x=age, fill=case)) +
+p1 <- ggplot(df, aes(x=age, fill=case)) +
     geom_histogram( color="#e9ecef", alpha=0.6, position = 'identity') +
     scale_fill_manual(values=c("#69b3a2", "#404080")) 
 
-  ggsave(p,dpi = 400,
-       filename="age_check.jpeg", path=here::here("output"),
+p2 <- ggplot(data=df, aes(x=age, group=case, fill=case)) +
+    geom_density(adjust=1.5, alpha=.4) 
+
+  ggsave(p1,dpi = 400,
+       filename="age_check_1.jpeg", path=here::here("output"),
+)  
+
+  ggsave(p2,dpi = 400,
+       filename="age_check_2.jpeg", path=here::here("output"),
 )  
