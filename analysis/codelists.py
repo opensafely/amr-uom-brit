0cad1ac932ecd16acff3ba87697786d1c54509fd
BillyZhongUOM <61462861+BillyZhongUOM@users.noreply.github.com>
2023-03-31 10:13:54 +0100

    update

--- analysis/codelists.py
+++ analysis/codelists.py
@@ -16,14 +16,13 @@ from cohortextractor import codelist, codelist_from_csv, combine_codelists
 # --- CODELISTS ---
 
 ### The following should be our own codelists in the future
-
-### All antibacterials
-antibacterials_codes= codelist_from_csv(
-  "codelists/opensafely-antibacterials.csv",
-  system = "snomed",
-  column = "dmd_id"
+ethnicity_codes = codelist_from_csv(
+    "codelists/opensafely-ethnicity.csv",
+    system="ctv3",
+    column="Code",
+    category_column="Grouping_6",
 )
-
+### All antibacterials
 
 antibacterials_codes_brit = codelist_from_csv(
     "codelists/user-BillyZhongUOM-brit_new_dmd.csv", system="snomed", column="dmd_id", category_column="type",
@@ -47,12 +46,6 @@ broad_spectrum_antibiotics_codes = codelist_from_csv(
 
 broad_spectrum_antibiotics_op = codelist_from_csv(
     "codelists/user-BillyZhongUOM-broad_spectrum_op.csv", system="snomed", column="dmd_id", category_column="type",
-)
-
-
-### asthma & COPD
-asthma_copd_codes = codelist_from_csv(
-    "codelists/user-rriefu-asthma_copd.csv", system="snomed", column="code"
 )
 
 
@@ -150,14 +143,6 @@ covrx_code = codelist_from_csv(
 )
 
 
-### asthma & COPD
-asthma_copd_codes= codelist_from_csv(
-  "codelists/user-rriefu-asthma_copd.csv",
-  system = "snomed",
-  column = "code"
-)
-
-
 ### asthma 
 asthma_codes= codelist_from_csv(
   "codelists/user-rriefu-asthma.csv",
@@ -167,23 +152,14 @@ asthma_codes= codelist_from_csv(
 
 
 ### cold
-cold_codes= codelist_from_csv(
-  "codelists/user-rriefu-cold_subset.csv",
-  system = "snomed",
-  column = "code"
-)
+cold_codes = codelist_from_csv(
+    "codelists/user-rriefu-cold_subset.csv", system="snomed", column="code")
+
 
 
 ### copd
 copd_codes= codelist_from_csv(
   "codelists/user-rriefu-copd.csv",
-  system = "snomed",
-  column = "code"
-)
-
-### cough & cold  
-cough_cold_codes= codelist_from_csv(
-  "codelists/user-rriefu-cough_cold.csv",
   system = "snomed",
   column = "code"
 )
@@ -276,8 +252,8 @@ uti_codes = codelist_from_csv(
 
 
 ## all infections
-all_indication_codes = combine_codelists(asthma_copd_codes, asthma_codes, cold_codes, copd_codes, 
-      cough_cold_codes, cough_codes, lrti_codes, ot_externa_codes, otmedia_codes, pneumonia_codes, 
+antibiotics_indications = combine_codelists(asthma_codes, cold_codes, copd_codes, 
+      cough_codes, lrti_codes, ot_externa_codes, otmedia_codes, pneumonia_codes, 
       renal_codes, sepsis_codes, sinusitis_codes, throat_codes, urti_codes, uti_codes )
 
 ## 6 common infection
@@ -385,99 +361,7 @@ hospitalisation_infection_related = codelist_from_csv(
   system="icd10",
   column="code",
 )
-##Charlson comobidities
-charlson01_cancer= codelist_from_csv(
-  "codelists/user-yayang-charlson01_cancer.csv",
-  system = "snomed",
-  column = "code"
-)
-charlson02_cvd= codelist_from_csv(
-  "codelists/user-yayang-charlson02_cvd.csv",
-  system = "snomed",
-  column = "code"
-)
-charlson03_copd= codelist_from_csv(
-  "codelists/user-yayang-charlson03_copd.csv",
-  system = "snomed",
-  column = "code"
-)
-charlson04_heart_failure= codelist_from_csv(
-  "codelists/user-yayang-charlson04_heart_failure.csv",
-  system = "snomed",
-  column = "code"
-)
-charlson05_connective_tissue= codelist_from_csv(
-  "codelists/user-yayang-charlson05_connective_tissue.csv",
-  system = "snomed",
-  column = "code"
-)
-charlson06_dementia= codelist_from_csv(
-  "codelists/user-yayang-charlson06_dementia.csv",
-  system = "snomed",
-  column = "code"
-)
-charlson07_diabetes= codelist_from_csv(
-  "codelists/user-yayang-charlson07_diabetes.csv",
-  system = "snomed",
-  column = "code"
-)
-charlson08_diabetes_with_complications= codelist_from_csv(
-  "codelists/user-yayang-charlson08_diabetes_with_complications.csv",
-  system = "snomed",
-  column = "code"
-)
-charlson09_hemiplegia= codelist_from_csv(
-  "codelists/user-yayang-charlson09_hemiplegia.csv",
-  system = "snomed",
-  column = "code"
-)
-charlson10_hiv= codelist_from_csv(
-  "codelists/user-yayang-charlson10_hiv.csv",
-  system = "snomed",
-  column = "code"
-)
-charlson11_metastatic_cancer= codelist_from_csv(
-  "codelists/user-yayang-charlson11_metastatic_cancer.csv",
-  system = "snomed",
-  column = "code"
-)
-charlson12_mild_liver= codelist_from_csv(
-  "codelists/user-yayang-charlson12_mild_liver.csv",
-  system = "snomed",
-  column = "code"
-)
-charlson13_mod_severe_liver= codelist_from_csv(
-  "codelists/user-yayang-charlson13_mod_severe_liver.csv",
-  system = "snomed",
-  column = "code"
-)
-charlson14_moderate_several_renal_disease= codelist_from_csv(
-  "codelists/user-yayang-charlson14_moderate_several_renaldiseae.csv",
-  system = "snomed",
-  column = "code"
-)
-charlson15_mi= codelist_from_csv(
-  "codelists/user-yayang-charlson15_mi.csv",
-  system = "snomed",
-  column = "code"
-)
-charlson16_peptic_ulcer= codelist_from_csv(
-  "codelists/user-yayang-charlson16_peptic_ulcer.csv",
-  system = "snomed",
-  column = "code"
-)
-charlson17_peripheral_vascular= codelist_from_csv(
-  "codelists/user-yayang-charlson17_peripheral_vascular.csv",
-  system = "snomed",
-  column = "code"
-)
 
-antibiotics_indications= codelist_from_csv(
-  "codelists/user-yayang-antibiotics_indications.csv",
-  system = "snomed",
-  column = "code",
-  category_column="category",
-)
 
 codes_ab_type_Trimethoprim= codelist_from_csv('codelists/user-yayang-codes_ab_type_trimethoprimcsv.csv', system ='snomed',column ='dmd_id', category_column="dmd_name")
 codes_ab_type_Nitrofurantoin= codelist_from_csv('codelists/user-yayang-codes_ab_type_nitrofurantoincsv.csv', system ='snomed',column ='dmd_id',category_column="dmd_name")
--- analysis/study_definition.py
+++ analysis/study_definition.py
@@ -111,7 +111,99 @@ study = StudyDefinition(
         },
     ),
 
-    
+   # index of multiple deprivation, estimate of SES based on patient post code 
+	imd=patients.categorised_as(
+        {
+            "0": "DEFAULT",
+            "1": """index_of_multiple_deprivation >=1 AND index_of_multiple_deprivation < 32844*1/5""",
+            "2": """index_of_multiple_deprivation >= 32844*1/5 AND index_of_multiple_deprivation < 32844*2/5""",
+            "3": """index_of_multiple_deprivation >= 32844*2/5 AND index_of_multiple_deprivation < 32844*3/5""",
+            "4": """index_of_multiple_deprivation >= 32844*3/5 AND index_of_multiple_deprivation < 32844*4/5""",
+            "5": """index_of_multiple_deprivation >= 32844*4/5 AND index_of_multiple_deprivation < 32844""",
+        },
+        index_of_multiple_deprivation=patients.address_as_of(
+            "index_date",
+            returning="index_of_multiple_deprivation",
+            round_to_nearest=100,
+        ),
+        return_expectations={
+            "rate": "universal",
+            "category": {
+                "ratios": {
+                    "0": 0.05,
+                    "1": 0.19,
+                    "2": 0.19,
+                    "3": 0.19,
+                    "4": 0.19,
+                    "5": 0.19,
+                }
+            },
+        },
+    ),   
+
+        # ETHNICITY IN 6 CATEGORIES
+    eth=patients.with_these_clinical_events(
+        ethnicity_codes,
+        returning="category",
+        on_or_before="index_date",
+        find_last_match_in_period=True,
+        include_date_of_match=False,
+        return_expectations={
+                                "category": {
+                                    "ratios": {
+                                        "1": 0.2,
+                                        "2": 0.2,
+                                        "3": 0.2,
+                                        "4": 0.2,
+                                        "5": 0.2
+                                        }
+                                    },
+                                "incidence": 0.75,
+                                },
+    ),
+
+    # fill missing ethnicity from SUS
+    ethnicity_sus=patients.with_ethnicity_from_sus(
+        returning="group_6",
+        use_most_frequent_code=True,
+        return_expectations={
+            "category": {
+                            "ratios": {
+                                "1": 0.2,
+                                "2": 0.2,
+                                "3": 0.2,
+                                "4": 0.2,
+                                "5": 0.2
+                                }
+                            },
+            "incidence": 0.4,
+            },
+    ),
+
+    ethnicity=patients.categorised_as(
+            {
+                "0": "DEFAULT",
+                "1": "eth='1' OR (NOT eth AND ethnicity_sus='1')",
+                "2": "eth='2' OR (NOT eth AND ethnicity_sus='2')",
+                "3": "eth='3' OR (NOT eth AND ethnicity_sus='3')",
+                "4": "eth='4' OR (NOT eth AND ethnicity_sus='4')",
+                "5": "eth='5' OR (NOT eth AND ethnicity_sus='5')",
+            },
+            return_expectations={
+                "category": {
+                                "ratios": {
+                                    "0": 0.5,  # missing in 50%
+                                    "1": 0.1,
+                                    "2": 0.1,
+                                    "3": 0.1,
+                                    "4": 0.1,
+                                    "5": 0.1
+                                    }
+                                },
+                "rate": "universal",
+            },
+    ),
+
     ### Sex
     sex=patients.sex(
         return_expectations={
@@ -299,6 +391,20 @@ measures = [
         numerator="broad_ab_count",
         denominator="antibiotic_count",
         group_by="region",
+    ),
+
+    Measure(
+        id="broad-spectrum-ratio_ethnicity",
+        numerator="broad_ab_count",
+        denominator="antibiotic_count",
+        group_by="ethnicity",
+    ),
+
+    Measure(
+        id="broad-spectrum-ratio_imd",
+        numerator="broad_ab_count",
+        denominator="antibiotic_count",
+        group_by="imd",
     ),
 
     Measure(
