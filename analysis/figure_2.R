05687afa66daff85e4452e069cf3626771634794
BillyZhongUOM <61462861+BillyZhongUOM@users.noreply.github.com>
2023-04-01 21:35:31 +0100

    update

--- analysis/figure_2.R
+++ analysis/figure_2.R
@@ -24,6 +24,16 @@ col_spec3 <-cols_only( value = col_number(),
                        date = col_date(format = "")
 )
 
+col_spec4 <-cols_only( value = col_number(),
+                       imd = col_character(),
+                       date = col_date(format = "")
+)
+
+col_spec5 <-cols_only( value = col_number(),
+                       ethnicity = col_character(),
+                       date = col_date(format = "")
+)
+
 lockdown_1_start = as.Date("2020-03-26")
 lockdown_1_end = as.Date("2020-06-01")
 lockdown_2_start = as.Date("2020-11-05")
@@ -64,7 +74,8 @@ figure_age_strata <- ggplot(df.plot, aes(x = as.Date("2019-01-01"), y = value, g
   annotate(geom = "rect", xmin = lockdown_3_start,xmax = lockdown_3_end,ymin = -Inf, ymax = Inf,fill="grey80", alpha=0.5)+
   geom_line(aes(x = date, y = value),size = 0.8)+ 
   scale_x_date(date_labels = "%Y %b", breaks = "3 months") +
-  labs(x = "", y = "rate of broad-sepctrum antibiotic", title = "", colour = "Age", fill = "Age") +
+  ylim(0, max(df.plot$value)) +
+  labs(x = "", y = "", title = "", colour = "Age", fill = "Age") +
   theme_bw() +
   theme(axis.title = element_text(size = 18),
         axis.text = element_text(size = 12),
@@ -95,7 +106,8 @@ figure_sex_strata <- ggplot(df2, aes(x = as.Date("2019-01-01"), y = value, group
   annotate(geom = "rect", xmin = lockdown_3_start,xmax = lockdown_3_end,ymin = -Inf, ymax = Inf,fill="grey80", alpha=0.5)+
   geom_line(aes(x = date, y = value),size = 0.8)+ 
   scale_x_date(date_labels = "%Y %b", breaks = "3 months") +
-  labs(x = "", y = "rate of broad-sepctrum antibiotic", title = "", colour = "Sex", fill = "Sex") +
+  ylim(0, max(df2$value)) +
+  labs(x = "", y = "", title = "", colour = "Sex", fill = "Sex") +
   theme_bw() +
   theme(axis.title = element_text(size = 18),
         axis.text = element_text(size = 12),
@@ -124,7 +136,8 @@ figure_region_strata <- ggplot(df3, aes(x = as.Date("2019-01-01"), y = value, gr
   annotate(geom = "rect", xmin = lockdown_3_start,xmax = lockdown_3_end,ymin = -Inf, ymax = Inf,fill="grey80", alpha=0.5)+
   geom_line(aes(x = date, y = value),size = 0.8)+ 
   scale_x_date(date_labels = "%Y %b", breaks = "3 months") +
-  labs(x = "", y = "rate of broad-sepctrum antibiotic", title = "", colour = "Region", fill = "Region") +
+  ylim(0, max(df3$value)) +
+  labs(x = "", y = "", title = "", colour = "Region", fill = "Region") +
   theme_bw() +
   theme(axis.title = element_text(size = 18),
         axis.text = element_text(size = 12),
@@ -137,15 +150,106 @@ figure_region_strata <- ggplot(df3, aes(x = as.Date("2019-01-01"), y = value, gr
   theme(axis.text.x=element_text(angle=60,hjust=1))+ scale_color_aaas()+ scale_fill_aaas()
 figure_region_strata
 
-ggsave(figure_age_strata, width = 12, height = 6, dpi = 640,
+### broad-spectrum rate by IMD
+
+df5 <- read_csv("measure_broad-spectrum-ratio_imd.csv",
+                col_types = col_spec4)
+df5$imd <- recode(df5$imd,
+       "0" = "NA", 
+       "1"= "IMD 1 (most deprived)",
+       "2"= "IMD 2",
+       "3"= "IMD 3",
+       "4"= "IMD 4",
+       "5"= "IMD 5 (least deprived)")
+df5 <- df5 %>% filter(!imd=="NA")
+df5$value <- round(df5$value,digits = 3)
+
+figure_imd_strata <- ggplot(df5, aes(x = as.Date("2019-01-01"), y = value, group = factor(imd), col = factor(imd), fill = factor(imd))) +
+  geom_boxplot(width=20, outlier.size=0, position="identity", alpha=.5) +
+  annotate(geom = "rect", xmin = lockdown_1_start,xmax = lockdown_1_end,ymin = -Inf, ymax = Inf,fill="grey80", alpha=0.5)+
+  annotate(geom = "rect", xmin = lockdown_2_start,xmax = lockdown_2_end,ymin = -Inf, ymax = Inf,fill="grey80", alpha=0.5)+
+  annotate(geom = "rect", xmin = lockdown_3_start,xmax = lockdown_3_end,ymin = -Inf, ymax = Inf,fill="grey80", alpha=0.5)+
+  geom_line(aes(x = date, y = value),size = 0.8)+ 
+  scale_x_date(date_labels = "%Y %b", breaks = "3 months") +
+  ylim(0, max(df5$value)) +
+  labs(x = "", y = "", title = "", colour = "IMD quintiles", fill = "IMD quintiles") +
+  theme_bw() +
+  theme(axis.title = element_text(size = 18),
+        axis.text = element_text(size = 12),
+        axis.text.x = element_text(angle = 60, hjust = 1),
+        legend.text = element_text(size = 12),
+        legend.title = element_text(size = 12),
+        legend.position = "top",
+        strip.background = element_rect(fill = "grey", colour =  NA),
+        strip.text = element_text(size = 12, hjust = 0)) +
+  theme(axis.text.x=element_text(angle=60,hjust=1))+ scale_color_aaas()+ scale_fill_aaas()
+figure_imd_strata
+
+### broad-spectrum rate by ethnicity
+
+df4 <- read_csv("measure_broad-spectrum-ratio_ethnicity.csv",
+                col_types = col_spec5)
+df4$ethnicity <- recode(df4$ethnicity,
+       "0" = "Unknown", 
+       "1"= "White",
+       "2"= "Mixed",
+       "3"= "South Asian",
+       "4"= "Black",
+       "5"= "Other")
+df4 <- df4 %>% filter(!ethnicity=="Unknown")
+df4$value <- round(df4$value,digits = 3)
+
+figure_ethnicity_strata <- ggplot(df4, aes(x = as.Date("2019-01-01"), y = value, group = factor(ethnicity), col = factor(ethnicity), fill = factor(ethnicity))) +
+  geom_boxplot(width=20, outlier.size=0, position="identity", alpha=.5) +
+  annotate(geom = "rect", xmin = lockdown_1_start,xmax = lockdown_1_end,ymin = -Inf, ymax = Inf,fill="grey80", alpha=0.5)+
+  annotate(geom = "rect", xmin = lockdown_2_start,xmax = lockdown_2_end,ymin = -Inf, ymax = Inf,fill="grey80", alpha=0.5)+
+  annotate(geom = "rect", xmin = lockdown_3_start,xmax = lockdown_3_end,ymin = -Inf, ymax = Inf,fill="grey80", alpha=0.5)+
+  geom_line(aes(x = date, y = value),size = 0.8)+ 
+  scale_x_date(date_labels = "%Y %b", breaks = "3 months") +
+  ylim(0, max(df4$value)) +
+  labs(x = "", y = "", title = "", colour = "Ethnicity", fill = "Ethnicity") +
+  theme_bw() +
+  theme(axis.title = element_text(size = 18),
+        axis.text = element_text(size = 12),
+        axis.text.x = element_text(angle = 60, hjust = 1),
+        legend.text = element_text(size = 12),
+        legend.title = element_text(size = 12),
+        legend.position = "top",
+        strip.background = element_rect(fill = "grey", colour =  NA),
+        strip.text = element_text(size = 12, hjust = 0)) +
+  theme(axis.text.x=element_text(angle=60,hjust=1))+ scale_color_aaas()+ scale_fill_aaas()
+figure_ethnicity_strata
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ggsave(figure_age_strata, width = 10, height = 6, dpi = 640,
        filename="figure_2.1.jpeg", path=here::here("output"),
 )  
 
 write_csv(df.plot, here::here("output", "figure_2.1_table.csv"))
 
-ggsave(figure_sex_strata, width = 12, height = 6, dpi = 640,
+ggsave(figure_sex_strata, width = 10, height = 6, dpi = 640,
        filename="figure_2.2.jpeg", path=here::here("output"),
 )  
-ggsave(figure_region_strata, width = 12, height = 6, dpi = 640,
+ggsave(figure_region_strata, width = 10, height = 6, dpi = 640,
        filename="figure_2.3.jpeg", path=here::here("output"),
+)  
+ggsave(figure_ethnicity_strata, width = 10, height = 6, dpi = 640,
+       filename="figure_2.4.jpeg", path=here::here("output"),
+)  
+
+ggsave(figure_imd_strata, width = 10, height = 6, dpi = 640,
+       filename="figure_2.5.jpeg", path=here::here("output"),
 )  
