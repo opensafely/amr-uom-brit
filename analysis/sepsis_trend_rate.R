023e54910bd3df1f37b6d25a4ba0ee2830137a03
BillyZhongUOM <61462861+BillyZhongUOM@users.noreply.github.com>
2023-04-20 11:09:54 +0100

    Update sepsis_trend_rate.R

--- analysis/sepsis_trend_rate.R
+++ analysis/sepsis_trend_rate.R
@@ -74,7 +74,7 @@ figure_imd_strata <- ggplot(df.plot, aes(x = as.Date("2019-01-01"), y = value, g
   annotate(geom = "rect", xmin = lockdown_3_start,xmax = lockdown_3_end,ymin = -Inf, ymax = Inf,fill="grey80", alpha=0.5)+
   geom_line(aes(x = monPlot, y = value),size = 0.8)+ 
   scale_x_date(date_labels = "%Y %b", breaks = "3 months") +
-  ylim(0, max(df.plot$value)) +
+  scale_y_continuous(limits = c(0, 0.5), breaks = seq(0, 0.5, by = 0.1), labels = function(x) sprintf("%0.1f", x)) +
   labs(x = "", y = "rate of spesis hospital admission", title = "", colour = "IMD", fill = "IMD") +
   theme_bw() +
   theme(axis.title = element_text(size = 18),
