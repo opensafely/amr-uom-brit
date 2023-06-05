601daea7367c3f5c01d73f8598f8dde9579ce067
BillyZhongUOM <61462861+BillyZhongUOM@users.noreply.github.com>
2023-06-05 16:28:22 +0100

    Update control_var_2_process.R

--- analysis/control_var_2_process.R
+++ analysis/control_var_2_process.R
@@ -15,22 +15,26 @@ library(readr)
 library(purrr)
 library(stringr)
 
+
+# Define the types and generate the input file names
 types <- c("uti", "lrti", "urti", "sinusitis", "ot_externa", "ot_media", "pneumonia")
 input_files <- lapply(1:9, function(i) {
   paste0("input_control_", i, "_", types, "_var_2.csv")
 })
 input_files <- unlist(input_files)
 
+# Function to process data
 process_data <- function(input_files, output_file) {
   # Define column names
   col=c("Rx_Amikacin", "Rx_Amoxicillin", "Rx_Ampicillin", "Rx_Azithromycin", "Rx_Aztreonam", "Rx_Benzylpenicillin", "Rx_Cefaclor", "Rx_Cefadroxil", "Rx_Cefalexin", "Rx_Cefamandole", "Rx_Cefazolin", "Rx_Cefepime", "Rx_Cefixime", "Rx_Cefotaxime", "Rx_Cefoxitin", "Rx_Cefpirome", "Rx_Cefpodoxime", "Rx_Cefprozil", "Rx_Cefradine", "Rx_Ceftazidime", "Rx_Ceftriaxone", "Rx_Cefuroxime", "Rx_Chloramphenicol", "Rx_Cilastatin", "Rx_Ciprofloxacin", "Rx_Clarithromycin", "Rx_Clindamycin", "Rx_Co_amoxiclav", "Rx_Co_fluampicil", "Rx_Colistimethate", "Rx_Dalbavancin", "Rx_Dalfopristin", "Rx_Daptomycin", "Rx_Demeclocycline", "Rx_Doripenem", "Rx_Doxycycline", "Rx_Ertapenem", "Rx_Erythromycin", "Rx_Fidaxomicin", "Rx_Flucloxacillin", "Rx_Fosfomycin", "Rx_Fusidate", "Rx_Gentamicin", "Rx_Levofloxacin", "Rx_Linezolid", "Rx_Lymecycline", "Rx_Meropenem", "Rx_Methenamine", "Rx_Metronidazole", "Rx_Minocycline", "Rx_Moxifloxacin", "Rx_Nalidixic_acid", "Rx_Neomycin", "Rx_Netilmicin", "Rx_Nitazoxanid", "Rx_Nitrofurantoin", "Rx_Norfloxacin", "Rx_Ofloxacin", "Rx_Oxytetracycline", "Rx_Phenoxymethylpenicillin", "Rx_Piperacillin", "Rx_Pivmecillinam", "Rx_Pristinamycin", "Rx_Rifaximin", "Rx_Sulfadiazine", "Rx_Sulfamethoxazole", "Rx_Sulfapyridine", "Rx_Taurolidin", "Rx_Tedizolid", "Rx_Teicoplanin", "Rx_Telithromycin", "Rx_Temocillin", "Rx_Tetracycline", "Rx_Ticarcillin", "Rx_Tigecycline", "Rx_Tinidazole", "Rx_Tobramycin", "Rx_Trimethoprim", "Rx_Vancomycin")
-
+  
+  # Generate full path to the input files
+  input_files_path <- here::here("output", input_files)
+  
   # Check if all input files exist
-  if (all(file.exists(here::here("output",input_files)))) {
+  if (all(file.exists(input_files_path))) {
     # Read all files
-    df_list <- lapply(input_files, function(input_file) {
-      df <- read_csv(here::here("output",input_file))
-      
+    df_list <- lapply(input_files_path, function(input_file) {
       # Data processing...
         # Data processing
     df[col] <- df[col] %>% mutate_all(~replace(., is.na(.), 0)) 
@@ -99,16 +103,18 @@ process_data <- function(input_files, output_file) {
     # Combine all data frames into one
     df_combined <- bind_rows(df_list)
 
-    # Save final dataset
-    saveRDS(df_combined, output_file)
+
+      # Save final dataset
+      saveRDS(df_combined, here::here("output", "processed", output_file))
+    })
   } else {
-    stop(paste("Some input files do not exist:", input_files[!file.exists(here::here("output",input_files))]))
+    stop(paste("Some input files do not exist:", input_files[!file.exists(input_files_path)]))
   }
 }
 
-types <- c("uti", "lrti", "urti", "sinusitis", "ot_externa", "ot_media", "pneumonia")
+# Apply function to each type
 for (type in types) {
   input_files_type <- grep(type, input_files, value = TRUE)
-  output_file <- paste0("output/processed/input_control_", type, "_var_2.rds")
+  output_file <- paste0("input_control_", type, "_var_2.rds")
   process_data(input_files_type, output_file)
 }
