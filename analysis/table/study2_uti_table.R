6b33edf15bbf3268af7615b1c9aea504291105e9
BillyZhongUOM <61462861+BillyZhongUOM@users.noreply.github.com>
2023-06-14 16:47:31 +0100

    update

---
+++ analysis/table/study2_uti_table.R
@@ -0,0 +1,20 @@
+# Load necessary library
+library(dplyr)
+library(readr)
+library(here)
+
+# Directory where input files are located
+input_dir <- here("output")
+
+# Load data from csv
+file_path <- file.path(input_dir, "matched_cases_uti2.csv")
+df <- read_csv(file_path)
+
+# Create a table of outcome_type counts
+outcome_table <- df %>%
+  group_by(outcome_type) %>%
+  summarise(count = n(), .groups = "keep")
+
+# Save the table as csv
+output_file_path <- file.path(here("output"), "study2_uti_table.csv")
+write_csv(outcome_table, output_file_path)
