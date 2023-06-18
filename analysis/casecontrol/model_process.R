459bf146f6c8d36f0e9e05989109316b7b187ae9
BillyZhongUOM <61462861+BillyZhongUOM@users.noreply.github.com>
2023-06-18 11:27:02 +0100

    Update model_process.R

--- analysis/casecontrol/model_process.R
+++ analysis/casecontrol/model_process.R
@@ -5,7 +5,7 @@ library("lubridate")
 library("here")
 
 # Function to define column specifications and load the datasets
-load_dataset <- function(file_path, condition) {
+load_dataset <- function(file_path) {
   col_spec <- cols_only(
     patient_index_date = col_date(format = ""),
     age = col_number(),
@@ -15,17 +15,15 @@ load_dataset <- function(file_path, condition) {
     patient_id = col_number()
   )
 
-  df <- read_csv(here(file_path, paste0("matched_cases_", condition, ".csv")), col_types = col_spec)
+  df <- read_csv(here(file_path), col_types = col_spec)
   return(df)
 }
-
 
 # Function to load and prepare the second and third dataset
-load_prepared_df <- function(file_path, selected_vars, condition) {
-  df <- readRDS(here(file_path, paste0("processed/input_case_", condition, "_abvar.rds"))) %>% select(one_of(selected_vars))
+load_prepared_df <- function(file_path, selected_vars) {
+  df <- readRDS(here(file_path)) %>% select(one_of(selected_vars))
   return(df)
 }
-
 
 # Function to merge datasets
 merge_data <- function(df.1, df.2, df.3) {
@@ -36,19 +34,19 @@ merge_data <- function(df.1, df.2, df.3) {
   return(merged_data)
 }
 
-# Main
+# Main function
 main <- function(condition) {
-  df_cases <- load_dataset("output", condition)
-  df_matches <- load_dataset("output", condition)
+  df_cases <- load_dataset(paste0("output/matched_cases_", condition, ".csv"))
+  df_matches <- load_dataset(paste0("output/matched_matches_", condition, ".csv"))
 
   df_ab_vars <- c("patient_index_date", "patient_id", "ab_treatment", "ab_frequency", "ab_history", "charlson_score", "charlsonGrp")
   df_oth_vars <- c("patient_index_date", "patient_id", "region", "imd", "ethnicity", "bmi", "smoking_status_comb", "ckd_rrt")
 
-  df_cases_ab <- load_prepared_df("output", df_ab_vars, condition)
-  df_cases_oth <- load_prepared_df("output", df_oth_vars, condition)
+  df_cases_ab <- load_prepared_df(paste0("output/processed/input_case_", condition, "_abvar.rds"), df_ab_vars)
+  df_cases_oth <- load_prepared_df(paste0("output/processed/input_case_", condition, "_othvar.rds"), df_oth_vars)
 
-  df_matches_ab <- load_prepared_df("output", df_ab_vars, condition)
-  df_matches_oth <- load_prepared_df("output", df_oth_vars, condition)
+  df_matches_ab <- load_prepared_df(paste0("output/processed/input_control_", condition, "_abvar.rds"), df_ab_vars)
+  df_matches_oth <- load_prepared_df(paste0("output/processed/input_control_", condition, "_othvar.rds"), df_oth_vars)
 
   df_cases <- merge_data(df_cases, df_cases_ab, df_cases_oth)
   df_matches <- merge_data(df_matches, df_matches_ab, df_matches_oth)
