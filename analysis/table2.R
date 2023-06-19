560fe774541637cd0d870b33ee936112ec36f027
rriefu <rriefu@gmail.com>
2023-06-19 21:46:55 +0100

    Update table2.R

--- analysis/table2.R
+++ analysis/table2.R
@@ -16,7 +16,7 @@ library("ggpubr")
 library("finalfit")
 
 ####### after matching ########
-DF=readRDS(here::here("output","all_rangers.rds"))
+DF=readRDS(here::here("output","all_ranger.rds"))
 
 DF=DF%>%dplyr::select("case","ethnicity_6",  "bmi_cat",  "CCI",  "smoking_cat_3","imd","care_home_type","care_home","covrx_ever","flu_vaccine",
                       "cancer_comor","cerebrovascular_disease_comor", "chronic_obstructive_pulmonary_comor", "heart_failure_comor", "connective_tissue_comor", "dementia_comor", "diabetes_comor", "diabetes_complications_comor", "hemiplegia_comor", "metastatic_cancer_comor", "mild_liver_comor", "mod_severe_liver_comor", "mod_severe_renal_comor", "mi_comor", "peptic_ulcer_comor", "peripheral_vascular_comor","hiv_comor",
