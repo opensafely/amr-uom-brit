e24d33908ab8039d70c50f6c0832810ccc4ceac9
rriefu <rriefu@gmail.com>
2023-06-20 09:16:24 +0100

    add comor

--- analysis/table2.R
+++ analysis/table2.R
@@ -18,9 +18,9 @@ library("finalfit")
 ####### after matching ########
 DF=readRDS(here::here("output","all_ranger.rds"))
 
-DF=DF%>%dplyr::select("case","ethnicity_6",  "bmi_cat",  "CCI",  "smoking_cat_3","imd","care_home_type","care_home","covrx_ever","flu_vaccine",
-                      "cancer_comor","cerebrovascular_disease_comor", "chronic_obstructive_pulmonary_comor", "heart_failure_comor", "connective_tissue_comor", "dementia_comor", "diabetes_comor", "diabetes_complications_comor", "hemiplegia_comor", "metastatic_cancer_comor", "mild_liver_comor", "mod_severe_liver_comor", "mod_severe_renal_comor", "mi_comor", "peptic_ulcer_comor", "peripheral_vascular_comor","hiv_comor",
-                      "total_ab_group","ab_types_group","exposure_period_group","recent_ab_days_group","broad_ab_prescriptions_group","interval_mean_group", "interval_sd_group")
+# DF=DF%>%dplyr::select("case","ethnicity_6",  "bmi_cat",  "CCI",  "smoking_cat_3","imd","care_home_type","care_home","covrx_ever","flu_vaccine",
+#                       "cancer_comor","cerebrovascular_disease_comor", "chronic_obstructive_pulmonary_comor", "heart_failure_comor", "connective_tissue_comor", "dementia_comor", "diabetes_comor", "diabetes_complications_comor", "hemiplegia_comor", "metastatic_cancer_comor", "mild_liver_comor", "mod_severe_liver_comor", "mod_severe_renal_comor", "mi_comor", "peptic_ulcer_comor", "peripheral_vascular_comor","hiv_comor",
+#                       "total_ab_group","ab_types_group","exposure_period_group","recent_ab_days_group","broad_ab_prescriptions_group","interval_mean_group", "interval_sd_group")
 DF$case=as.factor(DF$case)
 DF$imd=as.character(DF$imd)
 case.num=sum(DF$case==1)
@@ -77,33 +77,12 @@ round_tbl[,4]=plyr::round_any(round_tbl[,4], 5, f = round)
 
 write.csv(round_tbl,"table2_matched.csv")
 
-rm(list=ls())
 
 
-###### random sampling #######
-DF=readRDS(here::here("output","all_ranger.rds"))
+# random sampling
 DF=DF%>% dplyr::group_by(case,subclass)%>% sample_n(1)
-DF=DF%>% ungroup(case, subclass)
-DF=DF%>%dplyr::select("case","ethnicity_6",  "bmi_cat",  "CCI",  "smoking_cat_3","imd","care_home_type","care_home","covrx_ever","flu_vaccine",
-                     "cancer_comor","cerebrovascular_disease_comor", "chronic_obstructive_pulmonary_comor", "heart_failure_comor", "connective_tissue_comor", "dementia_comor", "diabetes_comor", "diabetes_complications_comor", "hemiplegia_comor", "metastatic_cancer_comor", "mild_liver_comor", "mod_severe_liver_comor", "mod_severe_renal_comor", "mi_comor", "peptic_ulcer_comor", "peripheral_vascular_comor","hiv_comor",
-                      "total_ab_group","ab_types_group",
-     "exposure_period_group","recent_ab_days_group",
-     "broad_ab_prescriptions_group",
-    "interval_mean_group", "interval_sd_group")
-DF$case=as.factor(DF$case)
-DF$imd=as.character(DF$imd)
-
-case.num=sum(DF$case==1)
-contr.num=sum(DF$case==0)
-
-
-# select variables
-explanatory<- c("ethnicity_6",  "bmi_cat",  "CCI",  "smoking_cat_3","imd","care_home","covrx_ever","flu_vaccine",
-               "cancer_comor","cerebrovascular_disease_comor", "chronic_obstructive_pulmonary_comor", "heart_failure_comor", "connective_tissue_comor", "dementia_comor", "diabetes_comor", "diabetes_complications_comor", "hemiplegia_comor", "metastatic_cancer_comor", "mild_liver_comor", "mod_severe_liver_comor", "mod_severe_renal_comor", "mi_comor", "peptic_ulcer_comor", "peripheral_vascular_comor","hiv_comor",
-                "total_ab_group","ab_types_group","exposure_period_group","recent_ab_days_group","broad_ab_prescriptions_group","interval_mean_group", "interval_sd_group")
-dependent <- "case"
-
 #table
+rm(tbl)
 tbl=DF%>% summary_factorlist(dependent, explanatory)
 
 round_tbl=tbl
@@ -118,31 +97,74 @@ round_tbl[,3]=plyr::round_any(round_tbl[,3], 5, f = round)
 round_tbl[,4]=as.numeric(round_tbl[,4])
 round_tbl[,4]=plyr::round_any(round_tbl[,4], 5, f = round)
 
-
-# #ethnicity
-# round_tbl[c(1:6),"percent_0"]=round_tbl[c(1:6),3]/sum(round_tbl[c(1:6),3])*100
-# round_tbl[c(1:6),"percent_1"]=round_tbl[c(1:6),4]/sum(round_tbl[c(1:6),4])*100
-
-# # bmi cat
-# round_tbl[c(7:11),"percent_0"]=round_tbl[c(7:11),3]/sum(round_tbl[c(7:11),3])*100
-# round_tbl[c(7:11),"percent_1"]=round_tbl[c(7:11),4]/sum(round_tbl[c(7:11),4])*100
-
-# #CCI
-# round_tbl[c(12:16),"percent_0"]=round_tbl[c(12:16),3]/sum(round_tbl[c(12:16),3])*100
-# round_tbl[c(12:16),"percent_1"]=round_tbl[c(12:16),4]/sum(round_tbl[c(12:16),4])*100
-
-# # smoking
-# round_tbl[c(17:20),"percent_0"]=round_tbl[c(17:20),3]/sum(round_tbl[c(17:20),3])*100
-# round_tbl[c(17:20),"percent_1"]=round_tbl[c(17:20),4]/sum(round_tbl[c(17:20),4])*100
-
-# # IMD
-# round_tbl[c(21:26),"percent_0"]=round_tbl[c(21:26),3]/sum(round_tbl[c(21:26),3])*100
-# round_tbl[c(21:26),"percent_1"]=round_tbl[c(21:26),4]/sum(round_tbl[c(21:26),4])*100
-
-# # care home, covid & flu vaccine
-
-# # commodities- 17 conditions
-# round_tbl[c(27:66),"percent_0"]=round_tbl[c(27:66),3]/contr.num
-# round_tbl[c(27:66),"percent_1"]=round_tbl[c(27:66),4]/case.num
-
 write.csv(round_tbl,"table2_random.csv")
+
+
+
+
+# ###### random sampling #######
+# DF=readRDS(here::here("output","all_ranger.rds"))
+# DF=DF%>% dplyr::group_by(case,subclass)%>% sample_n(1)
+
+# DF=DF%>%dplyr::select("case","ethnicity_6",  "bmi_cat",  "CCI",  "smoking_cat_3","imd","care_home_type","care_home","covrx_ever","flu_vaccine",
+#                      "cancer_comor","cerebrovascular_disease_comor", "chronic_obstructive_pulmonary_comor", "heart_failure_comor", "connective_tissue_comor", "dementia_comor", "diabetes_comor", "diabetes_complications_comor", "hemiplegia_comor", "metastatic_cancer_comor", "mild_liver_comor", "mod_severe_liver_comor", "mod_severe_renal_comor", "mi_comor", "peptic_ulcer_comor", "peripheral_vascular_comor","hiv_comor",
+#                       "total_ab_group","ab_types_group",
+#      "exposure_period_group","recent_ab_days_group",
+#      "broad_ab_prescriptions_group",
+#     "interval_mean_group", "interval_sd_group")
+# DF$case=as.factor(DF$case)
+# DF$imd=as.character(DF$imd)
+
+# case.num=sum(DF$case==1)
+# contr.num=sum(DF$case==0)
+
+
+# # select variables
+# explanatory<- c("ethnicity_6",  "bmi_cat",  "CCI",  "smoking_cat_3","imd","care_home","covrx_ever","flu_vaccine",
+#                "cancer_comor","cerebrovascular_disease_comor", "chronic_obstructive_pulmonary_comor", "heart_failure_comor", "connective_tissue_comor", "dementia_comor", "diabetes_comor", "diabetes_complications_comor", "hemiplegia_comor", "metastatic_cancer_comor", "mild_liver_comor", "mod_severe_liver_comor", "mod_severe_renal_comor", "mi_comor", "peptic_ulcer_comor", "peripheral_vascular_comor","hiv_comor",
+#                 "total_ab_group","ab_types_group","exposure_period_group","recent_ab_days_group","broad_ab_prescriptions_group","interval_mean_group", "interval_sd_group")
+# dependent <- "case"
+
+# #table
+# tbl=DF%>% summary_factorlist(dependent, explanatory)
+
+# round_tbl=tbl
+# #remove percentage
+# round_tbl[,3]=gsub("\\(.*?\\)","",round_tbl[,3])
+# round_tbl[,4]=gsub("\\(.*?\\)","",round_tbl[,4])
+
+# #round to 5
+# round_tbl[,3]=as.numeric(round_tbl[,3])
+# round_tbl[,3]=plyr::round_any(round_tbl[,3], 5, f = round)
+
+# round_tbl[,4]=as.numeric(round_tbl[,4])
+# round_tbl[,4]=plyr::round_any(round_tbl[,4], 5, f = round)
+
+
+# # #ethnicity
+# # round_tbl[c(1:6),"percent_0"]=round_tbl[c(1:6),3]/sum(round_tbl[c(1:6),3])*100
+# # round_tbl[c(1:6),"percent_1"]=round_tbl[c(1:6),4]/sum(round_tbl[c(1:6),4])*100
+
+# # # bmi cat
+# # round_tbl[c(7:11),"percent_0"]=round_tbl[c(7:11),3]/sum(round_tbl[c(7:11),3])*100
+# # round_tbl[c(7:11),"percent_1"]=round_tbl[c(7:11),4]/sum(round_tbl[c(7:11),4])*100
+
+# # #CCI
+# # round_tbl[c(12:16),"percent_0"]=round_tbl[c(12:16),3]/sum(round_tbl[c(12:16),3])*100
+# # round_tbl[c(12:16),"percent_1"]=round_tbl[c(12:16),4]/sum(round_tbl[c(12:16),4])*100
+
+# # # smoking
+# # round_tbl[c(17:20),"percent_0"]=round_tbl[c(17:20),3]/sum(round_tbl[c(17:20),3])*100
+# # round_tbl[c(17:20),"percent_1"]=round_tbl[c(17:20),4]/sum(round_tbl[c(17:20),4])*100
+
+# # # IMD
+# # round_tbl[c(21:26),"percent_0"]=round_tbl[c(21:26),3]/sum(round_tbl[c(21:26),3])*100
+# # round_tbl[c(21:26),"percent_1"]=round_tbl[c(21:26),4]/sum(round_tbl[c(21:26),4])*100
+
+# # # care home, covid & flu vaccine
+
+# # # commodities- 17 conditions
+# # round_tbl[c(27:66),"percent_0"]=round_tbl[c(27:66),3]/contr.num
+# # round_tbl[c(27:66),"percent_1"]=round_tbl[c(27:66),4]/case.num
+
+# write.csv(round_tbl,"table2_random.csv")
