348ff24f7e1d54c2596e6acd47369b4fdad512fb
BillyZhongUOM <61462861+BillyZhongUOM@users.noreply.github.com>
2023-06-21 10:52:07 +0100

    update

---
+++ analysis/casecontrol/model_table.R
@@ -0,0 +1,37 @@
+# Define your library imports
+library("tidyverse") 
+library('plyr')
+library('dplyr')
+library('lubridate')
+library('stringr')
+library("data.table")
+library("ggpubr")
+library("finalfit")
+
+columns <- c("ab_treatment", "ab_frequency", "ab_history", "charlson_score", "charlsonGrp")
+
+# Create a vector with the names of the datasets
+datasets <- c("uti", "lrti", "urti")
+
+# Loop over the names
+for (ds in datasets) {
+  
+  # Read the case and control datasets
+  data <- readRDS(here::here("output", "processed", paste0("model_", ds, ".rds")))
+  case <- data %>% filter(case==1)
+  control <- data %>% filter(case==0)
+
+
+  # Generate baseline table for case
+  case_summary <- case %>% summary_factorlist(explanatory = columns)
+
+  # Write case summary to csv
+  write_csv(case_summary, here::here("output", paste0("table_1_", ds, "_case.csv")))
+
+  # Generate baseline table for control
+  control_summary <- control %>% summary_factorlist(explanatory = columns)
+
+  # Write control summary to csv
+  write_csv(control_summary, here::here("output", paste0("table_1_", ds, "_control.csv")))
+
+}
