d85fdf91b934f911545d765bdab4a20f561662e3
BillyZhongUOM <61462861+BillyZhongUOM@users.noreply.github.com>
2023-06-26 16:48:09 +0100

    update

---
+++ analysis/matching_lrti_cdi.py
@@ -0,0 +1,16 @@
+from osmatching import match
+
+match(
+    case_csv="input_case_lrti_cdi",
+    match_csv="input_control_lrti_cdi",
+    matches_per_case=6,
+    match_variables={
+        "sex": "category",
+        "age": 5,
+        "patient_index_date": "month_only",
+    },
+    index_date_variable="patient_index_date",
+    closest_match_variables=["age"],
+    output_suffix="_lrti_cdi",
+    output_path="output",
+)
