7d8f23cfb1cd9f08f05c63051bf26763c4cc90d2
BillyZhongUOM <61462861+BillyZhongUOM@users.noreply.github.com>
2023-07-09 14:03:01 +0100

    update

---
+++ analysis/casecontrol/ab_freq_table.R
@@ -0,0 +1,58 @@
+# Load the necessary libraries
+library(readr)
+require('tidyverse')
+require("gtsummary")
+require("ggplot2")
+library("survival")
+library(car)
+library(data.table)
+library(gridExtra)
+library(here)
+
+main <- function(condition) {
+  # Read the dataset
+  df <- readRDS(here::here("output", "processed", paste0("model_", condition, "_ab.rds")))
+  df$case=as.numeric(df$case) #1/0
+
+  medications <- c("Rx_Amikacin", "Rx_Amoxicillin", "Rx_Ampicillin", "Rx_Azithromycin", "Rx_Aztreonam", 
+                  "Rx_Benzylpenicillin", "Rx_Cefaclor", "Rx_Cefadroxil", "Rx_Cefalexin", "Rx_Cefamandole", "Rx_Cefazolin", 
+                  "Rx_Cefepime", "Rx_Cefixime", "Rx_Cefotaxime", "Rx_Cefoxitin", "Rx_Cefpirome", "Rx_Cefpodoxime", 
+                  "Rx_Cefprozil", "Rx_Cefradine", "Rx_Ceftazidime", "Rx_Ceftriaxone", "Rx_Cefuroxime", "Rx_Chloramphenicol", 
+                  "Rx_Cilastatin", "Rx_Ciprofloxacin", "Rx_Clarithromycin", "Rx_Clindamycin", "Rx_Co_amoxiclav", 
+                  "Rx_Co_fluampicil", "Rx_Colistimethate", "Rx_Dalbavancin", "Rx_Dalfopristin", "Rx_Daptomycin", 
+                  "Rx_Demeclocycline", "Rx_Doripenem", "Rx_Doxycycline", "Rx_Ertapenem", "Rx_Erythromycin", "Rx_Fidaxomicin", 
+                  "Rx_Flucloxacillin", "Rx_Fosfomycin", "Rx_Fusidate", "Rx_Gentamicin", "Rx_Levofloxacin", "Rx_Linezolid", 
+                  "Rx_Lymecycline", "Rx_Meropenem", "Rx_Methenamine", "Rx_Metronidazole", "Rx_Minocycline", "Rx_Moxifloxacin", 
+                  "Rx_Nalidixic_acid", "Rx_Neomycin", "Rx_Netilmicin", "Rx_Nitazoxanid", "Rx_Nitrofurantoin", "Rx_Norfloxacin", 
+                  "Rx_Ofloxacin", "Rx_Oxytetracycline", "Rx_Phenoxymethylpenicillin", "Rx_Piperacillin", "Rx_Pivmecillinam", 
+                  "Rx_Pristinamycin", "Rx_Rifaximin", "Rx_Sulfadiazine", "Rx_Sulfamethoxazole", "Rx_Sulfapyridine", 
+                  "Rx_Taurolidin", "Rx_Tedizolid", "Rx_Teicoplanin", "Rx_Telithromycin", "Rx_Temocillin", "Rx_Tetracycline", 
+                  "Rx_Ticarcillin", "Rx_Tigecycline", "Rx_Tinidazole", "Rx_Tobramycin", "Rx_Trimethoprim", "Rx_Vancomycin")
+
+# Convert medication values to TRUE/FALSE
+  df <- df %>% 
+    mutate_at(vars(all_of(medications)), ~ if_else(. > 0, TRUE, FALSE))
+
+  # Generate the frequency table only for TRUE medication values
+  freq_table <- df %>% 
+    gather(medication, value, all_of(medications)) %>%
+    filter(value == TRUE) %>% 
+    group_by(medication, case) %>%  # group by both medication and case
+    summarise(count = n()) %>% 
+    mutate(percentage = count / sum(count) * 100) 
+
+  # Spread data into separate columns for case and control
+  freq_table <- freq_table %>% 
+    spread(key = case, value = count, fill = 0)
+  
+  # Remove "Rx_" prefix from medication names
+  freq_table$medication <- str_replace_all(freq_table$medication, "Rx_", "")
+  
+
+  # Save the frequency table
+  write_csv(freq_table, here::here("output", paste0(condition,"_medication_freq_table.csv")))
+}
+
+main("uti")
+main("urti")
+main("lrti")
