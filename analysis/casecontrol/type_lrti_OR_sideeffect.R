420647a2f531bbb03a83c507b12ccfb2c5be0c1f
BillyZhongUOM <61462861+BillyZhongUOM@users.noreply.github.com>
2023-08-03 11:14:44 +0100

    update

--- analysis/casecontrol/type_lrti_OR_sideeffect.R
+++ analysis/casecontrol/type_lrti_OR_sideeffect.R
@@ -9,45 +9,110 @@ library(data.table)
 library(gridExtra)
 library(here)
 
-main <- function(condition, medication) {
+main <- function(condition) {
   # Read the dataset
   df <- readRDS(here::here("output", "processed", paste0("model_", condition, "_ab.rds")))
-  setiddt <- read_csv(here::here("output",paste0("mapping_input_case_",condition,"_type.csv")))%>%select(set_id,outcome_type)
+  setiddt <- read_csv(here::here("output",paste0("mapping_input_case_",condition,"_type.csv")))%>%select(set_id,side_effect)
+
   # Preprocess the dataset
   df$case=as.numeric(df$case) #1/0
-  df$charlsonGrp= relevel(as.factor(df$charlsonGrp), ref="zero")
-  df$patient_index_date <- as.Date(df$patient_index_date, format = "%Y%m%d")
-
+  df$ethnicity= relevel(as.factor(df$ethnicity), ref="White")
+  df$region= relevel(as.factor(df$region), ref="East of England")
+  df$bmi= relevel(as.factor(df$bmi), ref="Healthy range (18.5-24.9 kg/m2)")
+  df$imd= relevel(as.factor(df$imd), ref="5 (least deprived)")
+  df$smoking_status_comb= relevel(as.factor(df$smoking_status_comb), ref="Never and unknown")
   df <- df %>% mutate(covid = case_when(patient_index_date < as.Date("2020-03-26") ~ "1",
                                         patient_index_date >=as.Date("2020-03-26") & patient_index_date < as.Date("2021-03-08") ~ "2",
                                         patient_index_date >= as.Date("2021-03-08") ~ "3"))
   df$covid=relevel(as.factor(df$covid), ref="1")
 
   df <- df %>% mutate(ab_history_count = case_when(ab_history == 0 ~ "0",
-                                                    ab_history == 1 ~ "1",
-                                                    ab_history > 1 & ab_history <3 ~ "2-3",
-                                                    ab_history >= 3 ~ "3+"))
+                                                  ab_history == 1 ~ "1",
+                                                  ab_history > 1 & ab_history <3 ~ "2-3",
+                                                  ab_history >= 3 ~ "3+"))
 
+  medications <- c("Rx_Amikacin", "Rx_Amoxicillin", "Rx_Ampicillin", "Rx_Azithromycin", "Rx_Aztreonam", 
+                  "Rx_Benzylpenicillin", "Rx_Cefaclor", "Rx_Cefadroxil", "Rx_Cefalexin", "Rx_Cefamandole", "Rx_Cefazolin", 
+                  "Rx_Cefepime", "Rx_Cefixime", "Rx_Cefotaxime", "Rx_Cefoxitin", "Rx_Cefpirome", "Rx_Cefpodoxime", 
+                  "Rx_Cefprozil", "Rx_Cefradine", "Rx_Ceftazidime", "Rx_Ceftriaxone", "Rx_Cefuroxime", "Rx_Chloramphenicol", 
+                  "Rx_Cilastatin", "Rx_Ciprofloxacin", "Rx_Clarithromycin", "Rx_Clindamycin", "Rx_Co_amoxiclav", 
+                  "Rx_Co_fluampicil", "Rx_Colistimethate", "Rx_Dalbavancin", "Rx_Dalfopristin", "Rx_Daptomycin", 
+                  "Rx_Demeclocycline", "Rx_Doripenem", "Rx_Doxycycline", "Rx_Ertapenem", "Rx_Erythromycin", "Rx_Fidaxomicin", 
+                  "Rx_Flucloxacillin", "Rx_Fosfomycin", "Rx_Fusidate", "Rx_Gentamicin", "Rx_Levofloxacin", "Rx_Linezolid", 
+                  "Rx_Lymecycline", "Rx_Meropenem", "Rx_Methenamine", "Rx_Metronidazole", "Rx_Minocycline", "Rx_Moxifloxacin", 
+                  "Rx_Nalidixic_acid", "Rx_Neomycin", "Rx_Netilmicin", "Rx_Nitazoxanid", "Rx_Nitrofurantoin", "Rx_Norfloxacin", 
+                  "Rx_Ofloxacin", "Rx_Oxytetracycline", "Rx_Phenoxymethylpenicillin", "Rx_Piperacillin", "Rx_Pivmecillinam", 
+                  "Rx_Pristinamycin", "Rx_Rifaximin", "Rx_Sulfadiazine", "Rx_Sulfamethoxazole", "Rx_Sulfapyridine", 
+                  "Rx_Taurolidin", "Rx_Tedizolid", "Rx_Teicoplanin", "Rx_Telithromycin", "Rx_Temocillin", "Rx_Tetracycline", 
+                  "Rx_Ticarcillin", "Rx_Tigecycline", "Rx_Tinidazole", "Rx_Tobramycin", "Rx_Trimethoprim", "Rx_Vancomycin")
 
-  df <- df %>% mutate(medication = case_when(df[[paste0("Rx_", medication)]] == 0 ~ FALSE,
-                                              df[[paste0("Rx_", medication)]] > 0 ~ TRUE))
+  # Convert medication values to TRUE/FALSE
+  # Define your medications
+  meds <- c("Rx_Amoxicillin", "Rx_Doxycycline", "Rx_Clarithromycin", "Rx_Co_amoxiclav")
+  other_meds <- setdiff(medications, meds) # Other medications
+
+  # Define the total count of TRUE medications
+  df$total_true <- rowSums(df[, medications])
+
+  # Define the total count of TRUE for other medications
+  df$other_true <- rowSums(df[, other_meds])
+
+  # Define medication variable
+  df <- df %>% mutate(
+    medication = case_when(
+      Rx_Amoxicillin == TRUE & total_true == 1 ~ "Amoxicillin",
+      Rx_Doxycycline == TRUE & total_true == 1 ~ "Doxycycline",
+      Rx_Clarithromycin == TRUE & total_true == 1 ~ "Clarithromycin",
+      Rx_Co_amoxiclav == TRUE & total_true == 1 ~ "Co_amoxiclav",
+      other_true == 1 & total_true == 1 ~ "Others",
+      total_true > 1 ~ "Multiple",
+      total_true == 0 ~ "None",
+      TRUE ~ NA_character_
+    )
+  )
+
   df <- merge(setiddt,df,by="set_id")
   df$set_id=as.factor(df$set_id) #pair id
-   # Filter the dataframe by outcome_type
-  df <- df %>% filter(outcome_type == "side effect")
+  # Filter the dataframe by outcome_type
+  df <- df %>% filter(side_effect == "1")
+
+  # Convert medication to factor, with "None" as the reference level
+  df$medication <- relevel(factor(df$medication), ref = "None")
+
+  # Calculate the frequency of each medication level by case
+  medication_freq <- df %>% group_by(medication, case) %>% summarise(freq = n())
+
+  # Pivot the data to have case values as columns
+  medication_freq_pivot <- medication_freq %>% pivot_wider(names_from = case, values_from = freq)
+
+  # Replace NA values with 0
+  medication_freq_pivot[is.na(medication_freq_pivot)] <- 0
+
+  # Write the frequency table to a CSV file
+  write_csv(medication_freq_pivot, here::here("output", paste0(condition,"_ab_medication_freq_sideeffect.csv")))
+
   # Initialize an empty list
   dfs <- list()
 
-
-  for (i in 1:2) {
-    
+  for (i in 1:5) {
+      
     if(i==1){
       mod=clogit(case ~ medication + strata(set_id), df)
     }
     else if(i==2){
-      mod=clogit(case ~ medication + ckd_rrt + strata(set_id), df)
+      mod=clogit(case ~ covid*medication + medication + covid + strata(set_id), df)
     }
-    
+    else if(i==3){
+      mod=clogit(case ~ medication + ethnicity + region + bmi + imd + smoking_status_comb + strata(set_id), df)
+    }
+    else if(i==4){
+      mod=clogit(case ~ medication + ethnicity + region + bmi + imd + smoking_status_comb + ckd_rrt + strata(set_id), df)
+    }
+    else if(i==5){
+      mod=clogit(case ~ medication + ethnicity + region + bmi + imd + smoking_status_comb + charlsonGrp + strata(set_id), df)
+    }
+
+
     sum.mod=summary(mod)
     result=data.frame(sum.mod$conf.int)
     DF=result[,-2]
@@ -77,13 +142,8 @@ main <- function(condition, medication) {
   combined_df <- combined_df %>% select(type, Model, `OR (95% CI)`)
 
   # Write the combined data frame to a CSV file
-  write_csv(combined_df, here::here("output", paste0(condition, "_", medication, "_OR_sideeffect.csv")))
+  write_csv(combined_df, here::here("output", paste0(condition,"_ab_type_OR_sideeffect.csv")))
 }
 
 
-# Call the main function for each condition
-medications <- c("Amoxicillin", "Doxycycline", "Clarithromycin", "Co_amoxiclav")
-
-for(medication in medications) {
-  main("lrti", medication)
-}
+main("lrti")
