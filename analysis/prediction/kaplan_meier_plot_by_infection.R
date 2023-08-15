1328135cfba5b63a6f49c4d296424bfd2d2071bf
BillyZhongUOM <61462861+BillyZhongUOM@users.noreply.github.com>
2023-08-15 11:29:02 +0100

    update

---
+++ analysis/prediction/kaplan_meier_plot_by_infection.R
@@ -0,0 +1,105 @@
+# Load the libraries
+library(here)
+library(dplyr)
+library(survival)
+library(ggplot2)
+library(tidyverse)
+
+data <- readRDS(here::here("output", "processed_data.rds"))
+
+# 2. Print the number of patients with chronic respiratory disease
+cat("Number of patients with chronic respiratory disease:", sum(data$has_chronic_respiratory_disease, na.rm = TRUE), "\n")
+
+# 3. Create the infection_indicator column
+data <- data %>% 
+  mutate(infection_indicator = has_uti | has_urti | has_lrti | has_sinusitis | has_ot_externa | has_otmedia)
+
+# 4. Print the number of patients with infection_indicator being TRUE
+cat("Number of patients with infection record:", sum(data$infection_indicator, na.rm = TRUE), "\n")
+
+# 5. Print the number of patients where covid_6weeks is FALSE
+cat("Number of patients without covid-19 positive record in six weeks:", sum(!data$covid_6weeks, na.rm = TRUE), "\n")
+
+# Remove patients with chronic respiratory disease
+data <- data %>% filter(!has_chronic_respiratory_disease)
+
+# Remove patients without infection_indicator
+data <- data %>% filter(infection_indicator)
+
+# Exclude patients with covid_6weeks being TRUE
+data <- data %>% filter(!covid_6weeks)
+
+
+# 6. Create the EVENT column
+data <- data %>% 
+  mutate(EVENT = ifelse(!is.na(emergency_admission_date), 1, 0))
+
+# 7. Print the number of patients where EVENT is 1 and 0
+event_table <- table(data$EVENT)
+cat("Number of patients where EVENT is 0:", event_table["0"], "\n")
+cat("Number of patients where EVENT is 1:", event_table["1"], "\n")
+
+# Create the variables
+
+# TtoAB
+data <- data %>%
+  mutate(TtoAB = ifelse(!is.na(ab_date_next), as.numeric(ab_date_next - patient_index_date), NA))
+
+# TtoD
+data <- data %>%
+  mutate(TtoD = ifelse(!is.na(died_any_date), as.numeric(died_any_date - patient_index_date), NA))
+
+# TtoAE
+data <- data %>%
+  mutate(TtoAE = ifelse(!is.na(emergency_admission_date), as.numeric(emergency_admission_date - patient_index_date), NA))
+
+# TtoEND
+data <- data %>%
+  mutate(TtoEND = as.numeric(as.Date("2020-03-25") - patient_index_date))
+
+# Create TEVENT column
+data <- data %>%
+  rowwise() %>%
+  mutate(TEVENT = min(c(TtoAB, TtoD, TtoAE, TtoEND), na.rm = TRUE)) %>%
+  ungroup() %>%
+  mutate(TEVENT = ifelse(TEVENT > 30, 30, TEVENT))
+
+
+# Define a vector of the column names
+infection_columns <- c("has_uti", "has_urti", "has_lrti", "has_sinusitis", "has_ot_externa", "has_otmedia")
+
+# Loop through each infection column
+for (col_name in infection_columns) {
+  # Filter data based on the current infection column
+  infection_data <- data %>% filter(get(col_name) == TRUE)
+
+  # Fit survival curve for this subset of data
+  fit <- survfit(Surv(TEVENT, EVENT) ~ 1, data = infection_data)
+  
+  surv_df <- data.frame(
+    time = fit$time,
+    survival = fit$surv,
+    upper = fit$upper,
+    lower = fit$lower
+  )
+  
+  # Generate the plot
+  plot_title <- tools::toTitleCase(sub("^has_", "", col_name))  # Convert 'has_uti' to 'Uti', etc.
+  Figure1 <- ggplot(surv_df, aes(x = time, y = survival)) +
+    geom_ribbon(aes(ymin = lower, ymax = upper), alpha = 0.3, fill = "blue") + 
+    geom_line(aes(y = survival), color = "blue", size = 0.5) +
+    labs(title = paste("Kaplan-Meier Survival Curve with Confidence Interval for", plot_title),
+         x = "Time (days)",
+         y = "Survival") +
+    theme_light() +
+    theme(legend.position = "topright") +
+    scale_y_continuous(expand = expansion(mult = c(0, 0.1)), 
+                       limits = c(0.99, 1),
+                       breaks = seq(0.99, 1, by = 0.001)) +
+    scale_x_continuous(expand = expansion(mult = c(0, 0.1))) + 
+    geom_blank(aes(y = 0.99))
+  
+  # Save the plot
+  filename <- paste0("KM_by_", plot_title, "_with_CI.jpeg")
+  ggsave(Figure1, dpi = 700, filename = filename, path = here::here("output"))
+}
