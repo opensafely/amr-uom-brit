e185f2a8d43d6228f7b307135655b6be5a8746f0
BillyZhongUOM <61462861+BillyZhongUOM@users.noreply.github.com>
2023-08-16 15:11:45 +0100

    update

---
+++ analysis/study_definition_case_2_cdi.py
@@ -0,0 +1,172 @@
+#### This script is to extract the cases: emergency admission & AE ICD-10  + had event 30 days before the index date
+
+
+from cohortextractor import (
+    StudyDefinition,
+    patients,
+    #codelist_from_csv,
+    codelist,
+    filter_codes_by_category,
+    #combine_codelists,
+    Measure
+)
+
+###### Code lists
+from codelists import *
+
+###### Define study time variables
+from datetime import datetime
+start_date = "2019-01-01"
+end_date = "2023-03-31"
+
+###### Cases definition ######
+## 1. age 18-110
+## 2. sex (M/F)
+## 3. at least one year of GP records prior to their index date
+## 4. has incident uti infeciton record (no any other uti infection record six weeks before)
+## 4. has ICD-10 related emergency admission within 30 days after the infection
+## 5. has no chronic res
+##############################
+
+study = StudyDefinition(
+
+    # Configure the expectations framework
+    default_expectations={
+        "date": {"earliest": start_date, "latest": end_date},
+        "rate": "uniform",
+        "incidence": 1,
+    },
+
+    # Set index date to start date
+    index_date=start_date,
+   
+    # study population
+    population=patients.satisfying(
+        """
+        NOT has_died
+        AND has_follow_up_previous_year
+        AND (sex = "M" OR sex = "F")
+        AND (age >=18 AND age <= 110)
+        AND has_outcome
+        AND NOT has_chronic_respiratory_disease
+        AND NOT has_outcome_previous_year
+        AND covid_6weeks = "0"
+        """,
+
+        has_died=patients.died_from_any_cause(
+            on_or_before="patient_index_date",
+            returning="binary_flag",
+        ),
+
+        has_follow_up_previous_year=patients.registered_with_one_practice_between(
+            start_date="patient_index_date - 365 days",
+            end_date="patient_index_date",
+            return_expectations={"incidence": 0.95},
+        ),
+
+        has_outcome=patients.with_these_clinical_events(
+            cdi_snomed,    
+            between=[start_date, end_date], 
+        ),
+
+        has_chronic_respiratory_disease=patients.with_these_clinical_events(
+            chronic_respiratory_disease_codes,  
+            returning="binary_flag",
+            on_or_before="patient_index_date",
+            find_last_match_in_period=True,
+        ),
+
+        has_outcome_previous_year=patients.with_these_clinical_events(
+            cdi_history_snomed,  
+            returning="binary_flag",
+            between=["patient_index_date- 366 days", "patient_index_date - 1 day"],
+            return_expectations={"incidence": 0.65},
+        ),
+
+    ),
+
+    ### patient index date 
+    patient_index_date=patients.with_these_clinical_events(
+        cdi_snomed,       
+        returning= "date",  
+        between=[start_date, end_date], 
+        find_first_match_in_period=True,  
+        date_format="YYYY-MM-DD",  
+        return_expectations={"date": {"earliest": "2019-01-01"}, "incidence" : 1},
+    ),
+
+    ## Age
+    age=patients.age_as_of(
+        "patient_index_date",
+        return_expectations={
+            "rate": "universal",
+            "int": {"distribution": "population_ages"},
+            "incidence": 0.001
+        },
+    ),
+    
+    ## Sex
+    sex=patients.sex(
+        return_expectations={
+            "rate": "universal",
+            "category": {"ratios": {"M": 0.49, "F": 0.51}},
+        }
+    ),
+
+    #Check COVID-diagnsis within +/- 6 weeks #
+
+    ## covid infection record sgss+gp ##
+    SGSS_positive_6weeks=patients.with_test_result_in_sgss(
+        pathogen="SARS-CoV-2",
+        test_result="positive",
+        between=["patient_index_date - 42 days","patient_index_date + 42 days"],
+        returning="binary_flag",
+        return_expectations={
+        "rate" : "exponential_increase",
+        "incidence" : 0.25},
+    ),
+    
+    GP_positive_6weeks=patients.with_these_clinical_events(
+        any_primary_care_code,        
+        returning="binary_flag",
+        between=["patient_index_date - 42 days","patient_index_date + 42 days"],
+         return_expectations={
+        "rate" : "exponential_increase",
+        "incidence" : 0.25},  ),
+
+        ## covid infection record hosp ##
+    covid_admission_6weeks=patients.admitted_to_hospital(
+        returning="binary_flag",
+        with_these_diagnoses=covid_codelist,
+        between=["patient_index_date - 42 days","patient_index_date + 42 days"],
+        return_expectations={
+        "rate" : "exponential_increase",
+        "incidence" : 0.25},    ),
+
+    covid_6weeks=patients.categorised_as(
+        {
+            "0": "DEFAULT",
+            "1": """
+                  SGSS_positive_6weeks OR GP_positive_6weeks OR covid_admission_6weeks
+            """,
+        },
+        return_expectations={
+                                "category": {
+                                    "ratios": {
+                                        "0": 0.8,
+                                        "1": 0.2
+                                        }
+                                    },
+                                },
+    ),
+
+    ab_history_3yr=patients.with_these_medications(
+        antibacterials_codes_brit,
+        between=["patient_index_date - 1185 days","patient_index_date - 90 days"],
+        returning="number_of_matches_in_period",
+        return_expectations={
+            "int": {"distribution": "normal", "mean": 3, "stddev": 1},
+            "incidence": 0.5,
+        },
+    ),
+)
