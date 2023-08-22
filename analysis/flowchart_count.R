fc72349628b6b5c02e85f31988503b596ae85b48
BillyZhongUOM <61462861+BillyZhongUOM@users.noreply.github.com>
2023-08-22 15:28:56 +0100

    Update flowchart_count.R

--- analysis/flowchart_count.R
+++ analysis/flowchart_count.R
@@ -7,41 +7,43 @@ library(here)
 # Read in dataset
 data <- read_csv(here::here("output", "input_flowchart.csv"))
 
-# Frequency tables for the specified columns
+# Function to print frequency table
+print_freq_table <- function(data, column_name) {
+  freq_table <- table(data[[column_name]])
+  cat(paste("Frequency Table for", column_name, ":\n"))
+  print(freq_table)
+  cat("\n")
+  return(data)
+}
+
+# Process data and print frequency tables
 
 # For has_follow_up
-has_follow_up_freq <- table(data$has_follow_up)
-cat("Frequency Table for has_follow_up:\n")
-print(has_follow_up_freq)
-cat("\n")
-
-# For age
-age_freq <- table(data$age)
-cat("Frequency Table for age:\n")
-print(age_freq)
-cat("\n")
-
-# For sex
-sex_freq <- table(data$sex)
-cat("Frequency Table for sex:\n")
-print(sex_freq)
-cat("\n")
-
-# For stp
-stp_freq <- table(data$stp)
-cat("Frequency Table for stp:\n")
-print(stp_freq)
-cat("\n")
-
-# For index_of_multiple_deprivation
-index_of_multiple_deprivation_freq <- table(data$index_of_multiple_deprivation)
-cat("Frequency Table for index_of_multiple_deprivation:\n")
-print(index_of_multiple_deprivation_freq)
+data %>% 
+  print_freq_table("has_follow_up") %>%
+  
+  # Filter out rows where has_follow_up = 0
+  filter(has_follow_up != 0) %>%
+  
+  # For age: print frequency table and then filter out ages not in [18, 110]
+  mutate(age_group = ifelse(age >= 18 & age <= 110, "18-110", "Other")) %>%
+  print_freq_table("age_group") %>%
+  filter(age_group == "18-110") %>%
+  
+  # For sex: print frequency table and then filter out sex "I" or "U"
+  print_freq_table("sex") %>%
+  filter(!(sex %in% c("I", "U"))) %>%
+  
+  # For stp: print frequency table
+  print_freq_table("stp") %>%
+  
+  # For index_of_multiple_deprivation
+  print_freq_table("index_of_multiple_deprivation")
 
 # Calculate and print row count
 row_count <- nrow(data)
-cat("Number of rows in the dataset:", row_count)
+cat("Number of rows in the original dataset:", row_count)
 
-row_count <- as.data.frame(row_count)
+row_count_df <- tibble(Row_Count = row_count)
 # Write to new csv
-write_csv(row_count, here::here("output", "flowchart_count.csv"))
+write_csv(row_count_df, here::here("output", "flowchart_count.csv"))
