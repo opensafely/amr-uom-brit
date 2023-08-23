3ddf8d4c94d23c7b21c5db2d7858f2fbd2aa532b
BillyZhongUOM <61462861+BillyZhongUOM@users.noreply.github.com>
2023-08-23 14:43:16 +0100

    Update cox_uti_model.R

--- analysis/prediction/cox_uti_model.R
+++ analysis/prediction/cox_uti_model.R
@@ -13,7 +13,6 @@ library(survival)
 library(rms)
 library(MASS)
 library(Hmisc)
-library("pseudo")
 
 ## Load data
 
@@ -317,7 +316,8 @@ data_km <- groupkm(pred_surv_prob, S = Surv(input_test$TEVENT,input_test$EVENT),
         g=10,u=time_point, pl=T, add=T,lty=0,cex.subtitle=FALSE)
 data_km <- as.data.frame(data_km)
 dev.off()
-
+head(data_km)
+summary(data_km)
 p1 <- ggplot(data_km, aes(x = x, y = KM)) +
   geom_point() +
   geom_abline(intercept = 0, slope = 1, color="red") +
@@ -331,12 +331,6 @@ p1
 
 ggsave(p1, dpi = 700,
        filename = "uti_30day_calibration.jpeg", path = here::here("output"))
-#jpeg(here::here("output", "uti_30day_calibration.jpeg"))
-#plot(val_ests,xlab="Expected Survival Probability",ylab="Observed Survival Probability") 
-#groupkm(pred_surv_prob, S = Surv(input_test$TEVENT,input_test$EVENT), 
-#        g=10,u=time_point, pl=T, add=T,lty=0,cex.subtitle=FALSE)
-#legend(0.0,0.8,c("Risk groups","Reference line","95% CI"),lty=c(0,2,1),pch=c(19,NA,NA),bty="n")
-#dev.off()
 
 print("Calibration plot is created successfully!")
 # Recalibration of the baseline survival function
@@ -353,13 +347,6 @@ sd(pred_surv_prob2)
 val_ests2 <- val.surv(est.surv = pred_surv_prob2,
                       S = Surv(input_test$TEVENT,input_test$EVENT), 
                       u=time_point,fun=function(p)log(-log(p)),pred = sort(runif(100, 0, 1)))
-
-#jpeg(here::here("output", "uti_30day_re-calibration.jpeg"))
-#plot(val_ests2,xlab="Expected Survival Probability",ylab="Observed Survival Probability") 
-#groupkm(pred_surv_prob2, S = Surv(input_test$TEVENT,input_test$EVENT), 
-#        g=10,u=time_point, pl=T, add=T,lty=0,cex.subtitle=FALSE)
-#legend(0.0,0.9,c("Risk groups","Reference line","95% CI"),lty=c(0,2,1),pch=c(19,NA,NA),bty="n")
-#dev.off()
 
 data_km2 <- groupkm(pred_surv_prob2, S = Surv(input_test$TEVENT,input_test$EVENT), 
         g=10,u=time_point, pl=T, add=T,lty=0,cex.subtitle=FALSE)
