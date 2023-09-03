a6a8982b9e813fa761c1bdeaf63db26689be8cb8
BillyZhongUOM <61462861+BillyZhongUOM@users.noreply.github.com>
2023-09-03 11:07:14 +0100

    Update trend_by_subgroup.R

--- analysis/prediction2/trend_by_subgroup.R
+++ analysis/prediction2/trend_by_subgroup.R
@@ -338,7 +338,7 @@ figure_imd_strata <- ggplot(df.plot, aes(x = as.Date("2019-01-01"), y = value, g
   annotate(geom = "rect", xmin = lockdown_3_start,xmax = lockdown_3_end,ymin = -Inf, ymax = Inf,fill="grey80", alpha=0.5)+
   geom_line(aes(x = monPlot, y = value),size = 0.8)+ 
   scale_x_date(date_labels = "%Y %b", breaks = seq(as.Date("2019-01-01"), as.Date("2023-07-01"), by = "3 months")) +
-  scale_y_continuous(limits = c(0, 0.01), breaks = seq(0, 0.01, by = 0.001), labels = function(x) sprintf("%0.001f", x)) +
+  scale_y_continuous(limits = c(0, 0.01), breaks = seq(0, 0.01, by = 0.001), labels = function(x) sprintf("%0.3f", x)) +
   labs(x = "", y = "", title = "", colour = "IMD", fill = "IMD") +
   theme_bw() +
   theme(axis.title = element_text(size = 18),
