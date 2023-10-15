59a6159b4b570eb773305c1b10de3eea69869fe1
BillyZhongUOM <61462861+BillyZhongUOM@users.noreply.github.com>
2023-10-15 10:52:53 +0100

    udpate

---
+++ analysis/a_impute_ch.R
@@ -0,0 +1,62 @@
+### This script is for sensitivity analysis -- complete case analysis ###
+
+## Import libraries---
+
+require('tidyverse')
+require("gtsummary")
+require("ggplot2")
+library("survival")
+library(car)
+library(data.table)
+library(gridExtra)
+library(here)
+library(mice)
+
+
+df <- readRDS("output/processed/input_model_c_h.rds")
+df$smoking_status[df$smoking_status == "Missing"] <- NA
+df$bmi_adult[df$bmi_adult == "Missing"] <- NA
+df$ethnicity[df$ethnicity == "Unknown"] <- NA
+
+df$imd= relevel(as.factor(df$imd), ref="5")
+
+df <- df %>% select(case, imd ,ethnicity ,bmi_adult ,smoking_status ,hypertension ,chronic_respiratory_disease,
+             asthma ,chronic_cardiac_disease ,diabetes_controlled ,cancer ,haem_cancer ,chronic_liver_disease,
+             stroke ,dementia ,other_neuro ,organ_kidney_transplant ,asplenia ,ra_sle_psoriasis ,immunosuppression,
+             learning_disability ,sev_mental_ill ,alcohol_problems ,care_home_type_ba ,ckd_rrt ,ab_frequency, set_id)
+
+set.seed(123) # Setting a seed for reproducibility
+
+## Select 1% of the data randomly
+df_sampled <- df 
+
+## Function to calculate and print the number of missing values in each column
+print_missing_values <- function(df){
+  missing_values <- sapply(df, function(x) sum(is.na(x)))
+  print(missing_values)
+}
+
+## Print the number of missing values before imputation in the sampled data
+cat("Number of missing values before imputation in the sampled data:\n")
+print_missing_values(df_sampled)
+
+## Perform mice imputation on sampled data
+imp_sampled <- mice(df_sampled, maxit=0, seed = 123)
+predM_sampled <- imp_sampled$predictorMatrix
+meth_sampled <- imp_sampled$method
+
+
+predM_sampled[, c("set_id")] <- 0
+
+imp2_sampled <- mice(df_sampled, maxit = 5, seed = 123,
+                     predictorMatrix = predM_sampled, 
+                     method = meth_sampled, print = TRUE)
+
+df_imp_long_sampled <- mice::complete(imp2_sampled, action="long", include = TRUE)            
+
+## Print the number of missing values after imputation in the sampled data
+cat("Number of missing values after imputation in the sampled data:\n")
+print_missing_values(df_imp_long_sampled)
+
+write.csv(df_imp_long_sampled, here::here("output", "imputation_dataframe_ch.csv"))
+
---
+++ analysis/sensitivity/sen_imputed_crude_demo.R
@@ -0,0 +1,56 @@
+require('tidyverse')
+require("gtsummary")
+require("ggplot2")
+library("survival")
+library(car)
+library(data.table)
+library(gridExtra)
+library(here)
+library(mice)
+
+df_imp_long_ch <- read_csv (here::here ("output", "imputation_dataframe_ch.csv"))
+df_imp_long_ch_mids<-as.mids(df_imp_long_ch)
+df_imp_long_c <- read_csv (here::here ("output", "imputation_dataframe_c.csv"))
+df_imp_long_c_mids<-as.mids(df_imp_long_c)
+df_imp_long_h <- read_csv (here::here ("output", "imputation_dataframe_h.csv"))
+df_imp_long_h_mids<-as.mids(df_imp_long_h)
+
+calculate_ORs <- function(data_mids, variable) {
+  model <- with(data_mids,
+                clogit(case ~ get(variable) + strata(set_id)))
+  model <- summary(pool(model)) 
+  
+  # Extracting coefficients and other details from the model
+  coefs <- model$estimate
+  std_errors <- model$std.error
+  var_names <- model$term
+  
+  # Calculating Odds Ratios and 95% CI
+  ORs <- exp(coefs)
+  lower_95 <- exp(coefs - 1.96 * std_errors)
+  upper_95 <- exp(coefs + 1.96 * std_errors)
+  
+  # Creating a data frame with results
+  results <- data.frame(Variable = var_names,
+                        Odds_Ratios = ORs,
+                        Lower_95_CI = lower_95,
+                        Upper_95_CI = upper_95)
+  
+  return(results)
+}
+
+# List of datasets
+datasets <- list(ch = df_imp_long_ch_mids, 
+                 c = df_imp_long_c_mids, 
+                 h = df_imp_long_h_mids)
+
+# Applying the function to each variable of interest
+variables <- c("imd", "ethnicity", "bmi_adult", "smoking_status")
+
+# Iterating over datasets and variables
+for (data_name in names(datasets)) {
+  for (var in variables) {
+    result_df <- calculate_ORs(datasets[[data_name]], var)
+    write_csv(result_df, here::here("output", paste0("imputed_crude_", data_name, "_", var, ".csv")))
+  }
+}
